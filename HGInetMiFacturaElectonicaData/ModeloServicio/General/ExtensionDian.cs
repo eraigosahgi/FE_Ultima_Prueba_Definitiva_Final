48ea9437cdeeb7cc30fbfab03d87d8dadfbde4e6
Mparamo <mparamo@noemail.com>
2018-03-01 16:05:40 +0000

    Se agregan propiedades en la ExtensionDian para soportar la generación del elemento XML
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@8 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectonicaData/ModeloServicio/General/ExtensionDian.cs
+++ HGInetMiFacturaElectonicaData/ModeloServicio/General/ExtensionDian.cs
@@ -12,20 +12,30 @@ namespace HGInetMiFacturaElectonicaData.ModeloServicio
 	public class ExtensionDian
 	{
 
-		/// <summary>
-		/// Tipo de Documento Segun la DIAN { get; set; } 1-Factura, 2-Nota Credito, 3-Nota Debito
-		/// </summary>
-		public int TipoDocumento { get; set; }
+        /// <summary>
+        /// Código del País de Expedición del documento (CO)
+        /// </summary>
+        public string CodPaisExpedicion { get { return "CO"; } }
+
+        /// <summary>
+        /// Tipo de Documento Segun la DIAN { get; set; } 1-Factura, 2-Nota Credito, 3-Nota Debito
+        /// </summary>
+        public int TipoDocumento { get; set; }
 
 		/// <summary>
-		/// Numero de Resolucion de Factura
+		/// Número de Resolución del documento Factura*
 		/// </summary>
 		public string NumResolucion { get; set; }
 
-		/// <summary>
-		/// Fecha Inicio de Resolucion de Factura
+        /// <summary>
+		/// Prefijo del documento Factura*
 		/// </summary>
-		public DateTime FechaResIni { get; set; }
+		public string Prefijo { get; set; }
+
+        /// <summary>
+        /// Fecha Inicio de Resolucion de Factura*
+        /// </summary>
+        public DateTime FechaResIni { get; set; }
 
 		/// <summary>
 		/// Fecha Final de Resolucion de Factura
@@ -41,11 +51,17 @@ namespace HGInetMiFacturaElectonicaData.ModeloServicio
 		/// Rango Final de Resolucion de Factura
 		/// </summary>
 		public int RangoFin { get; set; }
-
-		/// <summary>
-		/// Identificador del software proporcionado en la plataforma de la DIAN
+        
+        /// <summary>
+		/// Número de Identificación del Proveedor Tecnológico
 		/// </summary>
-		public string IdSoftware { get; set; }
+		public string NitProveedor { get; set; }
+
+
+        /// <summary>
+        /// Identificador del software proporcionado en la plataforma de la DIAN
+        /// </summary>
+        public string IdSoftware { get; set; }
 
 		/// <summary>
 		/// Pin del software proporcionado en la plataforma de la DIAN
