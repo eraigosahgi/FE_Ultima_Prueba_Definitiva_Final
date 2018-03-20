120f038582a19a545049b453f3749bbba7ad43d5
Mparamo <mparamo@noemail.com>
2018-03-20 18:19:06 +0000

    Revisión de las propiedades del objeto Tercero de acuerdo con el manual técnico
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@35 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectonicaData/ModeloServicio/General/Tercero.cs
+++ HGInetMiFacturaElectonicaData/ModeloServicio/General/Tercero.cs
@@ -7,130 +7,124 @@ using System.Threading.Tasks;
 
 namespace HGInetMiFacturaElectonicaData.ModeloServicio
 {
-    public class Tercero
-    {
+	/// <summary>
+	/// Representación del Facturador Electrónico y del Adquiriente
+	/// </summary>
+	public class Tercero
+	{
 
-        /// <summary>
-        /// Identificacion
-        /// Campo: StrIdTercero       
-        /// </summary>
-        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es obligatorio")]
-        [RegularExpression("^\\d+$", ErrorMessage = "La {0} debe contener sólo números.")]
-        public string Identificacion { get; set; }
+		/// <summary>
+		/// Número de identificación
+		/// </summary>
+		[Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es obligatorio")]
+		[RegularExpression("^\\d+$", ErrorMessage = "La {0} debe contener sólo números.")]
+		public string Identificacion { get; set; }
 
-        /// <summary>
-        /// Digito de Verificación de identificacion
-        /// </summary>
-        [Required(ErrorMessage = "{0} es un campo obligatorio")]
-        [Range(0, 9, ErrorMessage = "El valor de {0} debe estar entre {1} y {2}")]
-        public int IdentificacionDv { get; set; }
+		/// <summary>
+		/// Digito de verificación del número de identificación
+		/// </summary>
+		[Required(ErrorMessage = "{0} es un campo obligatorio")]
+		[Range(0, 9, ErrorMessage = "El valor de {0} debe estar entre {1} y {2}")]
+		public int IdentificacionDv { get; set; }
 
-        /// <summary>
-        /// Tipo de Documento: 13-Cedula, 22-Cedula Extranjeria, 31-NIT
-        /// </summary>
-        [Required(ErrorMessage = "{0} es un campo obligatorio")]
-        [Range(0, 99, ErrorMessage = "El valor de {0} debe estar entre {1} y {2}")]
-        public int TipoIdentificacion { get; set; }
+		/// <summary>
+		/// Tipo de identificación: 13-Cedula, 22-Cedula Extranjeria, 31-NIT
+		/// Anexo Técnico 001 Formatos de los Documentos XML de Facturación Electrónica.pdf
+		/// </summary>
+		[Required(ErrorMessage = "{0} es un campo obligatorio")]
+		[Range(0, 99, ErrorMessage = "El valor de {0} debe estar entre {1} y {2}")]
+		public int TipoIdentificacion { get; set; }
 
-        /// <summary>
-        /// Tipo de Persona Juridica: 1-Juridica, 2-Natural
-        /// Campo: IntTipoPersona
-        /// </summary>
-        [Required(ErrorMessage = "{0} es un campo obligatorio")]
-        [Range(1, 2, ErrorMessage = "El valor de {0} debe estar entre {1} y {2}")]
-        public int TipoPersona { get; set; }
+		/// <summary>
+		/// Tipo de Persona: 1-Juridica, 2-Natural
+		/// </summary>
+		[Required(ErrorMessage = "{0} es un campo obligatorio")]
+		[Range(1, 2, ErrorMessage = "El valor de {0} debe estar entre {1} y {2}")]
+		public int TipoPersona { get; set; }
 
-        /// <summary>
-        /// Regimen Tributario: 0-Simplificado, 2-Común
-        /// Campo: IntRegimen
-        /// </summary>
-        [Required(ErrorMessage = "{0} es un campo obligatorio")]
-        [Range(0, 2, ErrorMessage = "El valor de {0} debe estar entre {1} y {2}")]
-        public int Regimen { get; set; }
+		/// <summary>
+		/// Regimen Tributario: 0-Simplificado, 2-Común
+		/// </summary>
+		[Required(ErrorMessage = "{0} es un campo obligatorio")]
+		[Range(0, 2, ErrorMessage = "El valor de {0} debe estar entre {1} y {2}")]
+		public int Regimen { get; set; }
 
-        /// <summary>
-        /// Nombre Comercial
-        /// Campo: StrNombre
-        /// </summary>
-        public string NombreComercial { get; set; }
+		/// <summary>
+		/// Nombre Comercial
+		/// </summary>
+		public string NombreComercial { get; set; }
 
-        /// <summary>
-        /// Departamento
-        /// </summary>
-        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es obligatorio")]
-        public string Departamento { get; set; }
+		/// <summary>
+		/// Departamento
+		/// </summary>
+		[Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es obligatorio")]
+		public string Departamento { get; set; }
 
-        /// <summary>
-        /// Ciudad
-        /// Campo: StrCiudad
-        /// </summary>
-        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es obligatorio")]
-        public string Ciudad { get; set; }
+		/// <summary>
+		/// Ciudad
+		/// </summary>
+		[Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es obligatorio")]
+		public string Ciudad { get; set; }
 
-        /// <summary>
-        /// Direccion
-        /// Campo: StrDireccion
-        /// </summary>
-        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es obligatorio")]
-        public string Direccion { get; set; }
+		/// <summary>
+		/// Direccion
+		/// </summary>
+		[Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es obligatorio")]
+		public string Direccion { get; set; }
 
-        /// <summary>
-        /// Telefono
-        /// </summary>
-        [Required(ErrorMessage = "{0} es un campo obligatorio")]
-        [StringLength(10, MinimumLength = 7,
-        ErrorMessage = "La propiedad {0} debe tener {1} caracteres de máximo y {2} de mínimo")]
-        [RegularExpression("^\\d+$", ErrorMessage = "El Teléfono debe contener sólo números.")]
-        public string Telefono { get; set; }
+		/// <summary>
+		/// Telefono
+		/// </summary>
+		[Required(ErrorMessage = "{0} es un campo obligatorio")]
+		[StringLength(10, MinimumLength = 7,
+		ErrorMessage = "La propiedad {0} debe tener {1} caracteres de máximo y {2} de mínimo")]
+		[RegularExpression("^\\d+$", ErrorMessage = "El Teléfono debe contener sólo números.")]
+		public string Telefono { get; set; }
 
-        /// <summary>
-        /// Correo 
-        /// </summary>
-        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es obligatorio")]
-        [RegularExpression("\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*", ErrorMessage = "Mail incorrecto")]
-        public string Mail { get; set; }
+		/// <summary>
+		/// Correo 
+		/// </summary>
+		[Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es obligatorio")]
+		[RegularExpression("\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*", ErrorMessage = "Mail incorrecto")]
+		public string Email { get; set; }
 
-        /// <summary>
-        /// Pagina Web
-        /// </summary>
-        public string PaginaWeb { get; set; }
+		/// <summary>
+		/// Pagina Web
+		/// </summary>
+		public string PaginaWeb { get; set; }
 
-        /// <summary>
-        /// Pais: Segun tabla DIAN 
-        /// </summary>
-        [MaxLength(2, ErrorMessage = "El {0} no puede superar los {1} caracteres")]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es obligatorio")]
-        public string CodigoPais { get; set; }
+		/// <summary>
+		/// Código del País según ISO 3166-1 alfa-2 (ej: CO = Colombia, etc.)
+		/// </summary>
+		[MaxLength(2, ErrorMessage = "El {0} no puede superar los {1} caracteres")]
+		[Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es obligatorio")]
+		public string CodigoPais { get; set; }
 
-        /// <summary>
-        /// Razon Social
-        /// Campo: StrApellido1
-        /// </summary>
-        public string RazonSocial { get; set; }
+		/// <summary>
+		/// Razón Social
+		/// </summary>
+		public string RazonSocial { get; set; }
 
-        /// <summary>
-        /// Primer Apellido
-        /// Campo: StrApellido1-StrApellido2
-        /// </summary>
-        public string PrimerApellido { get; set; }
+		/// <summary>
+		/// Primer Apellido
+		/// </summary>
+		public string PrimerApellido { get; set; }
 
-        /// <summary>
-        /// Segundo Apellido
-        /// </summary>
-        public string SegundoApellido { get; set; }
+		/// <summary>
+		/// Segundo Apellido
+		/// </summary>
+		public string SegundoApellido { get; set; }
 
-        /// <summary>
-        /// Primer Nombre
-        /// Campo: StrNombre1
-        /// </summary>
-        public string PrimerNombre { get; set; }
+		/// <summary>
+		/// Primer Nombre
+		/// </summary>
+		public string PrimerNombre { get; set; }
 
-        /// <summary>
-        /// Segundo nombre
-        /// Campo: StrNombre2
-        /// </summary>
-        public string SegundoNombre { get; set; }
+		/// <summary>
+		/// Segundo nombre
+		/// </summary>
+		public string SegundoNombre { get; set; }
 
 
-    }
+	}
 }
