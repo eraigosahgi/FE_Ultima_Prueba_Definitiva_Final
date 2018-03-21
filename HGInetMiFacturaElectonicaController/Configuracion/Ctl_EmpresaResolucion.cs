fb4c0eb0d151814a9becd120eeac3c7282929f72
Mparamo <mparamo@noemail.com>
2018-03-21 14:31:38 +0000

    - Se genera el controlador para generar el xml en estandar UBL - Se genera el controlador para obtener la resolución de la empresa desde la base de datos - Se crea el archivo para el controlador de la firma del documento XML
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@37 30a10932-8c21-4e4d-906a-15261f94da71

---
+++ HGInetMiFacturaElectonicaController/Configuracion/Ctl_EmpresaResolucion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HGInetMiFacturaElectonicaData;
+using HGInetMiFacturaElectonicaData.ControllerSql;
+using HGInetMiFacturaElectonicaData.Modelo;
+
+namespace HGInetMiFacturaElectonicaController.Configuracion
+{
+	public class Ctl_EmpresaResolucion : BaseObject<TblEmpresasResoluciones>
+	{
+		#region Constructores 
+
+		public Ctl_EmpresaResolucion() : base(new ModeloAutenticacion()) { }
+		public Ctl_EmpresaResolucion(ModeloAutenticacion autenticacion) : base(autenticacion) { }
+
+		public Ctl_EmpresaResolucion(string servidor, string basedatos, string usuario, string clave) : base(servidor, basedatos, usuario, clave) { }
+		#endregion
+
+		/// <summary>
+		/// Obtiene la resolución de una empresa
+		/// </summary>
+		/// <param name="documento_empresa">documento de la empresa</param>
+		/// <param name="numero_resolucion">número de resolución</param>
+		/// <returns>datos de la resolución</returns>
+		public TblEmpresasResoluciones Obtener(string documento_empresa, string numero_resolucion)
+		{
+			try
+			{
+				var datos = (from item in context.TblEmpresasResoluciones
+							where item.StrNumResolucion.Equals(numero_resolucion)
+							&& item.TblEmpresas.StrIdentificacion.Equals(documento_empresa)
+							select item).FirstOrDefault();
+
+				if (datos == null)
+					throw new ApplicationException(string.Format("No se encontró el número de resolución {0} para el documento {1}.", numero_resolucion, documento_empresa));
+								
+				return datos;
+			}
+			catch (Exception exec)
+			{
+				throw new ApplicationException(exec.Message);
+			}
+		}
+	}
+}
---
+++ HGInetMiFacturaElectonicaController/Procesos/Ctl_Ubl.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HGInetMiFacturaElectonicaController.Configuracion;
+using HGInetMiFacturaElectonicaData;
+using HGInetMiFacturaElectonicaData.Modelo;
+using HGInetMiFacturaElectonicaData.ModeloServicio;
+using HGInetUBL;
+using HGInetUBL.Objetos;
+
+namespace HGInetMiFacturaElectonicaController.Procesos
+{
+	/// <summary>
+	/// Controlador para gestionar la generación del estándar UBL
+	/// </summary>
+	public class Ctl_Ubl
+	{
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="documento"></param>
+		/// <param name="tipo"></param>
+		/// <returns></returns>
+		public ResultadoXml Generar(Factura documento, TipoDocumento tipo)
+		{
+			// obtiene la resolución del documento
+			Ctl_EmpresaResolucion _resolucion = new Ctl_EmpresaResolucion();
+			TblEmpresasResoluciones resolucion_documento = _resolucion.Obtener(documento.DatosObligado.Identificacion, documento.NumeroResolucion);
+
+			// obtiene los datos del proveedor tecnológico de la DIAN
+			DianProveedor data_dian = HgiConfiguracion.GetConfiguration().DianProveedor;
+
+			// convierte la información de la resolución a la extensión DIAN
+			HGInetMiFacturaElectonicaData.ModeloServicio.ExtensionDian extension_documento = new HGInetMiFacturaElectonicaData.ModeloServicio.ExtensionDian()
+			{
+				TipoDocumento = tipo.GetHashCode(),
+				NumResolucion = resolucion_documento.StrNumResolucion,
+				Prefijo = resolucion_documento.StrPrefijo,
+				FechaResIni = resolucion_documento.DatFechaVigenciaDesde,
+				FechaResFin = resolucion_documento.DatFechaVigenciaHasta,
+				RangoIni = resolucion_documento.IntRangoInicial,
+				RangoFin = resolucion_documento.IntRangoFinal,
+				IdSoftware = data_dian.IdSoftware,
+				NitProveedor = data_dian.NitProveedor,
+				ClaveTecnicaDIAN = data_dian.ClaveAmbiente,
+				PinSoftware = data_dian.Pin
+			};
+
+			// convierte el documento 
+			ResultadoXml resultado = FacturaXML.CrearDocumento(documento, extension_documento);
+
+			return resultado;
+		}
+
+
+	}
+}
