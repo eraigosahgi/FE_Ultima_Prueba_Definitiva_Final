636c2bd33af281c6556db821aeb9df37c2c41ecb
Mparamo <mparamo@noemail.com>
2018-03-22 19:10:21 +0000

    Se crea el controlador para ejecutar el servicio web de resoluciones de la Dian
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@43 30a10932-8c21-4e4d-906a-15261f94da71

---
+++ HGInetMiFacturaElectonicaController/ServiciosDian/Ctl_ResolucionDian.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibreriaGlobalHGInet.Funciones;
+using LibreriaGlobalHGInet.General;
+
+namespace HGInetMiFacturaElectonicaController.ServiciosDian
+{
+	public class Ctl_ResolucionDian
+	{
+		public static string Obtener(Guid id_peticion, string id_software, string clave, string nit_empresa, string nit_proveedor)
+		{
+			try
+			{
+				// Producción HGI SAS
+				HGInetDIANServicios.DianResolucion.ResolucionesFacturacion resoluciones_produccion = HGInetDIANServicios.Ctl_Resolucion.Obtener(id_peticion, id_software, clave, nit_empresa, nit_proveedor, Fecha.GetFecha());
+
+				string carpeta = Directorio.CrearDirectorio(AppDomain.CurrentDomain.BaseDirectory + @"_LogFacturaE\");
+
+				string archivo = carpeta + id_peticion.ToString() + ".xml";
+
+				string xml_doc = System.IO.File.ReadAllText(archivo);
+				
+				return xml_doc;
+
+			}
+			catch (Exception exc)
+			{
+				throw exc;
+			}
+
+
+		}
+
+	}
+}
