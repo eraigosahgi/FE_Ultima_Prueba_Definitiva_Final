f1c81f1be8c5c267c1bb1fe6fd577840537764c1
Mparamo <mparamo@noemail.com>
2018-03-26 15:55:29 +0000

    - Documentación - Se agrega el parámetro pruebas al método para indicar si el documento es de habilitación DIAN - Se crea el método para almacenar físicamente el documento en Xml
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@63 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectonicaController/Procesos/Ctl_Ubl.cs
+++ HGInetMiFacturaElectonicaController/Procesos/Ctl_Ubl.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HGInetMiFacturaElectonicaController.Configuracion;
+using HGInetMiFacturaElectonicaController.ServiciosDian;
 using HGInetMiFacturaElectonicaData;
 using HGInetMiFacturaElectonicaData.Modelo;
 using HGInetMiFacturaElectonicaData.ModeloServicio;
 using HGInetUBL;
 using HGInetUBL.Objetos;
+using LibreriaGlobalHGInet.General;
 using LibreriaGlobalHGInet.Objetos;
 
 namespace HGInetMiFacturaElectonicaController.Procesos
@@ -20,46 +22,142 @@ namespace HGInetMiFacturaElectonicaController.Procesos
 	{
 
 		/// <summary>
-		/// 
+		/// Genera la información del documento en formato UBL
 		/// </summary>
-		/// <param name="documento"></param>
-		/// <returns></returns>
-		public static FacturaE_Documento Generar(Factura documento)
+		/// <param name="id">id único de identificación de la plataforma</param>
+		/// <param name="documento">datos del documento</param>
+		/// <param name="pruebas">indica si el documento es de pruebas (true)</param>
+		/// <returns>datos del documento</returns>
+		public static FacturaE_Documento Generar(Guid id, Factura documento, bool pruebas = false)
 		{
 			TipoDocumento tipo = TipoDocumento.Factura;
 
-			// obtiene la resolución del documento
-			Ctl_EmpresaResolucion _resolucion = new Ctl_EmpresaResolucion();
-			TblEmpresasResoluciones resolucion_documento = _resolucion.Obtener(documento.DatosObligado.Identificacion, documento.NumeroResolucion);
+			// resolución del documento
+			HGInetMiFacturaElectonicaData.ModeloServicio.ExtensionDian extension_documento = new HGInetMiFacturaElectonicaData.ModeloServicio.ExtensionDian();
 
-			// obtiene los datos del proveedor tecnológico de la DIAN
-			DianProveedor data_dian = HgiConfiguracion.GetConfiguration().DianProveedor;
-
-			// convierte la información de la resolución a la extensión DIAN
-			HGInetMiFacturaElectonicaData.ModeloServicio.ExtensionDian extension_documento = new HGInetMiFacturaElectonicaData.ModeloServicio.ExtensionDian()
+			if (pruebas)
 			{
-				TipoDocumento = tipo.GetHashCode(),
-				NumResolucion = resolucion_documento.StrNumResolucion,
-				Prefijo = resolucion_documento.StrPrefijo,
-				FechaResIni = resolucion_documento.DatFechaVigenciaDesde,
-				FechaResFin = resolucion_documento.DatFechaVigenciaHasta,
-				RangoIni = resolucion_documento.IntRangoInicial,
-				RangoFin = resolucion_documento.IntRangoFinal,
-				IdSoftware = data_dian.IdSoftware,
-				NitProveedor = data_dian.NitProveedor,
-				ClaveTecnicaDIAN = data_dian.ClaveAmbiente,
-				PinSoftware = data_dian.Pin
-			};
+				/*
+					# Resolucion : 9000000033394696 
+					Desde 990000000
+					Hasta 995000000
+					Identificador 606f5740-c6b9-494f-931c-5a6b3e22d72c
+					Pin Pfe2017
+					Clave Tecnica dd85db55545bd6566f36b0fd3be9fd8555c36e
+					Clave Ambiente Prueba2017
+					Ruta Servicio https://facturaelectronica.dian.gov.co/habilitacion/B2BIntegrationEngine/FacturaElectronica/facturaElectronica.wsdl
+				 */
+
+				// obtiene los datos de prueba del proveedor tecnológico de la DIAN
+				DianProveedorTest data_dian = HgiConfiguracion.GetConfiguration().DianProveedorTest;
+
+				//Ctl_ResolucionDian.Obtener(id, data_dian.IdSoftware, data_dian.ClaveAmbiente, documento.DatosObligado.Identificacion, data_dian.NitProveedor);
+
+				// convierte la información de la resolución a la extensión DIAN
+				extension_documento = new HGInetMiFacturaElectonicaData.ModeloServicio.ExtensionDian()
+				{
+					TipoDocumento = tipo.GetHashCode(),
+					NumResolucion = "9000000033394696",
+					Prefijo = "",
+					FechaResIni = new DateTime(2017, 07, 02),
+					FechaResFin = new DateTime(2027, 07, 02),
+					RangoIni = 990000000,
+					RangoFin = 995000000,
+					IdSoftware = data_dian.IdSoftware,
+					NitProveedor = data_dian.NitProveedor,
+					ClaveTecnicaDIAN = "dd85db55545bd6566f36b0fd3be9fd8555c36e",
+					PinSoftware = data_dian.Pin
+				};
+			}
+			else
+			{
+				// obtiene los datos del proveedor tecnológico de la DIAN
+				DianProveedor data_dian = HgiConfiguracion.GetConfiguration().DianProveedor;
+
+
+
+			}
 
 			// convierte el documento 
 			FacturaE_Documento resultado = FacturaXML.CrearDocumento(documento, extension_documento);
+			resultado.DocumentoTipo = tipo;
+			resultado.ID = id;
 
-			// Obtiene el nombre del archivo ZIP
+			// genera el nombre del archivo ZIP
 			resultado.NombreZip = NombramientoArchivo.ObtenerZip(documento.Documento.ToString(), documento.DatosObligado.Identificacion, tipo);
-			
+
 			return resultado;
 		}
 
+		public static FacturaE_Documento Generar(Guid id, NotaCredito documento, bool pruebas = false)
+		{
+			return null;
+		}
+
+		public static FacturaE_Documento Generar(Guid id, NotaDebito documento, bool pruebas = false)
+		{
+			return null;
+		}
+
+		/// <summary>
+		/// Almacena el archivo Xml físicamente
+		/// </summary>
+		/// <param name="documento">datos del documento</param>
+		/// <returns>datos del documento</returns>
+		public static FacturaE_Documento Almacenar(FacturaE_Documento documento)
+		{
+			System.IO.StreamWriter file = null;
+
+			try
+			{
+				
+				string nit_obligado = string.Empty;
+
+				switch (documento.DocumentoTipo)
+				{
+					case TipoDocumento.Factura:
+						Factura doc_factura = ((Factura)documento.Documento);
+						nit_obligado = doc_factura.DatosObligado.Identificacion;
+						break;
+					case TipoDocumento.NotaCredito:
+						NotaCredito doc_nota_credito = ((NotaCredito)documento.Documento);
+						nit_obligado = doc_nota_credito.DatosObligado.Identificacion;
+						break;
+					case TipoDocumento.NotaDebito:
+						/*NotaDebito doc_nota_debito = ((NotaDebito)documento.Documento);
+						nit_obligado = doc_nota_debito.DatosObligado.Identificacion;
+						*/
+						break;
+					default:
+						break;
+				}
+
+				// ruta del xml
+				string ruta_xml = LibreriaGlobalHGInet.Dms.ObtenerCarpetaPrincipal(Directorio.ObtenerDirectorioRaiz(), nit_obligado);
+				ruta_xml = string.Format(@"{0}\{1}.xml", ruta_xml, LibreriaGlobalHGInet.Properties.RecursoDms.CarpetaXmlFacturaE, documento.NombreXml);
+				
+				// elimina el archivo xml si existe
+				if (Archivo.ValidarExistencia(ruta_xml))
+					Archivo.Borrar(ruta_xml);
+				
+				// almacena el archivo xml
+				using (file = new System.IO.StreamWriter(ruta_xml))
+				{
+					file.WriteLine(documento.DocumentoXml.ToString());
+				}
+
+				return documento;
+			}
+			catch (Exception excepcion)
+			{
+				throw excepcion;
+			}
+			finally
+			{
+				if (file != null)
+					file.Close();
+			}
+		}
 
 	}
 }
