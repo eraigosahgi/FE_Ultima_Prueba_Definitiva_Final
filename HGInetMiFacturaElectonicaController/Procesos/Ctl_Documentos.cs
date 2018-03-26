99a260f02e960957cf0dcecbb448f549d0038a20
Mparamo <mparamo@noemail.com>
2018-03-26 15:56:50 +0000

    - Se genera el controlador para realizar los procesos del documento en la plataforma - Se genera el controlador para el envío de los documento a la DIAN
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@64 30a10932-8c21-4e4d-906a-15261f94da71

---
+++ HGInetMiFacturaElectonicaController/Procesos/Ctl_Documentos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HGInetFirmaDigital;
+using HGInetMiFacturaElectonicaController.ServiciosDian;
+using HGInetMiFacturaElectonicaData.ModeloServicio;
+using LibreriaGlobalHGInet.General;
+using LibreriaGlobalHGInet.Objetos;
+
+namespace HGInetMiFacturaElectonicaController.Procesos
+{
+	/// <summary>
+	/// Controlador para gestionar los documentos
+	/// </summary>
+	public class Ctl_Documentos
+	{
+
+		/// <summary>
+		/// Realiza el proceso de la plataforma para el documento
+		/// 1. Generar UBL - 2. Firmar - 3. Almacenar XML - 4. Comprimir - 5. Enviar DIAN
+		/// </summary>
+		/// <param name="id_peticion">id único de identificación de la plataforma</param>
+		/// <param name="documento_obj">datos del documento</param>
+		/// <param name="pruebas">indica si el documento es de pruebas (true)</param>
+		/// <returns>datos de resultado para el documento</returns>
+		public static FacturaE_Documento Procesar(Guid id_peticion, Factura documento_obj, bool pruebas = false)
+		{
+			// genera el xml en ubl
+			FacturaE_Documento documento_result = Ctl_Ubl.Generar(id_peticion, documento_obj, pruebas);
+
+			// firma el xml 
+			string ruta_certificado = string.Format("{0}{1}", Directorio.ObtenerDirectorioRaiz(), "certificado_test.p12");
+			documento_result = Ctl_Firma.Generar(ruta_certificado, "6c 0b 07 62 62 6d a0 e2", "persona_juridica_pruebas1", EnumCertificadoras.Andes, documento_result);
+
+			// almacena el xml
+			documento_result = Ctl_Ubl.Almacenar(documento_result);
+
+			// comprime el archivo xml
+			Ctl_Compresion.Comprimir(documento_result);
+
+			// envia el documento a la DIAN
+			HGInetDIANServicios.DianFactura.AcuseRecibo acuse = Ctl_DocumentoDian.Enviar(documento_result);
+
+			return documento_result;
+
+		}
+
+	}
+}
---
+++ HGInetMiFacturaElectonicaController/ServiciosDian/Ctl_DocumentoDian.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HGInetDIANServicios;
+using HGInetDIANServicios.DianFactura;
+using HGInetMiFacturaElectonicaData;
+using HGInetMiFacturaElectonicaData.ModeloServicio;
+using LibreriaGlobalHGInet.General;
+using LibreriaGlobalHGInet.Objetos;
+
+namespace HGInetMiFacturaElectonicaController.ServiciosDian
+{
+	public class Ctl_DocumentoDian
+	{
+
+		public static AcuseRecibo Enviar(FacturaE_Documento documento)
+		{
+			// obtiene los datos del proveedor tecnológico de la DIAN
+			DianProveedorTest data_dian = HgiConfiguracion.GetConfiguration().DianProveedorTest;
+			
+			string prefijo = string.Empty;
+
+			string numero = string.Empty;
+
+			string nit_obligado = string.Empty;
+
+			DateTime fecha = new DateTime();
+
+			switch (documento.DocumentoTipo)
+			{
+				case TipoDocumento.Factura:
+					Factura doc_factura= ((Factura)documento.Documento);
+					prefijo = doc_factura.Prefijo;
+					numero = doc_factura.Documento.ToString();
+					nit_obligado = doc_factura.DatosObligado.Identificacion;
+					fecha = doc_factura.Fecha;
+					break;
+				case TipoDocumento.NotaCredito:
+					NotaCredito doc_nota_credito = ((NotaCredito)documento.Documento);
+					numero = doc_nota_credito.Documento.ToString();
+					nit_obligado = doc_nota_credito.DatosObligado.Identificacion;
+					fecha = doc_nota_credito.Fecha;
+					break;
+				case TipoDocumento.NotaDebito:
+					/*NotaDebito doc_nota_debito = ((NotaDebito)documento.Documento);
+					numero = doc_nota_debito.Documento.ToString();
+					nit_obligado = doc_nota_debito.DatosObligado.Identificacion;
+					fecha = doc_nota_debito.Fecha;*/
+					break;
+				default:
+					break;
+			}
+			
+			// ruta del zip
+			string ruta_zip = LibreriaGlobalHGInet.Dms.ObtenerCarpetaPrincipal(Directorio.ObtenerDirectorioRaiz(), nit_obligado);
+			ruta_zip = string.Format(@"{0}\{1}.zip", ruta_zip, LibreriaGlobalHGInet.Properties.RecursoDms.CarpetaXmlFacturaE, documento.NombreZip);
+
+			// envía el documento a través de los servicios web de la DIAN
+			AcuseRecibo acuse = Ctl_Factura.Enviar(documento.ID, data_dian.IdSoftware, data_dian.ClaveAmbiente, prefijo, numero, fecha, nit_obligado, ruta_zip, data_dian.UrlServicioWeb);
+
+			return acuse;
+		}
+
+	}
+}
