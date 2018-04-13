0bb4495f921bff1ef240474d6dc4d927fe64022f
Mparamo <mparamo@noemail.com>
2018-04-13 21:29:10 +0000

    Prueba de expresión regular para el decimal
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@105 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetFacturaETestConsola/Program.cs
+++ HGInetFacturaETestConsola/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace HGInetFacturaETestConsola
 {
@@ -9,6 +10,45 @@ namespace HGInetFacturaETestConsola
 	{
 		static void Main(string[] args)
 		{
+
+			Regex isnumber = new Regex(@"^(0|([1-9][0-9]*))(\.\d\d$)$");
+
+			//  ^[0-9]+(\\,[0-9]{2,2})?$
+
+			//  ^[0-9]([.,][0-9]{1,3})?$
+
+			//  [0-9]+(\.[0-9][0-9]?)?
+
+			//  ^\d+(\.\d{1,2})?$
+
+			// @"\d{1,12}\.\d\d$"
+
+			//  @"(^(0|([1-9][0-9]*))(\.[0-9]{1,2})?$)|(^(0{0,1}|([1-9][0-9]*))(\.[0-9]{2,2})?$)"
+
+			decimal valor = 102.3m;
+			string valor_txt = Convert.ToString(valor).Replace(",", ".");
+
+			bool decimal1 = isnumber.IsMatch(valor_txt);
+
+
+			valor = 2365478523102.30m;
+			valor_txt = Convert.ToString(valor).Replace(",", ".");
+
+			bool decimal2 = isnumber.IsMatch(valor_txt);
+
+
+
+			valor = 102.307m;
+			valor_txt = Convert.ToString(valor).Replace(",", ".");
+
+			bool decimal3 = isnumber.IsMatch(valor_txt);
+
+
+			valor = 0.30m;
+			valor_txt = Convert.ToString(valor).Replace(",", ".");
+
+			bool decimal4 = isnumber.IsMatch(valor_txt);
+
 		}
 	}
 }
