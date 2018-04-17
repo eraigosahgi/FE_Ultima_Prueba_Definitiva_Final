aa57bb6a101da4d698d38898009938487a0b630a
Mparamo <mparamo@noemail.com>
2018-04-17 18:03:19 +0000

    Se crea el controlador de acceso a la base de datos para TblDocumentos
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@123 30a10932-8c21-4e4d-906a-15261f94da71

---
+++ HGInetMiFacturaElectonicaController/Registros/Ctl_Documento.cs
@@ -0,0 +1,40 @@
+﻿using HGInetMiFacturaElectonicaData;
+using HGInetMiFacturaElectonicaData.ControllerSql;
+using HGInetMiFacturaElectonicaData.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HGInetMiFacturaElectonicaController.Registros
+{
+	public class Ctl_Documento : BaseObject<TblDocumentos>
+	{
+		#region Constructores 
+
+		public Ctl_Documento() : base(new ModeloAutenticacion()) { }
+		public Ctl_Documento(ModeloAutenticacion autenticacion) : base(autenticacion) { }
+
+		public Ctl_Documento(string servidor, string basedatos, string usuario, string clave) : base(servidor, basedatos, usuario, clave) { }
+		#endregion
+
+
+		public TblDocumentos Crear(TblDocumentos documento)
+		{
+			documento = this.Add(documento);
+
+			return documento;
+		}
+
+
+		public void Validar(TblDocumentos documento)
+		{
+			
+
+		}
+
+	}
+
+
+}
