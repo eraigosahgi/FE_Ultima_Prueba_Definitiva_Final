236a20874c59b82d1cf12f081d684cea5dea586d
Atamayo <atamayo.hgi@gmail.com>
2018-04-21 15:22:21 +0000

    se realiza la implementación de los NuGets AngularJS, JQuery y DevExtreme. se genera página con contenido para demos (DataGrid, Formularios, Botones, Modales y mensajes.)
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@127 30a10932-8c21-4e4d-906a-15261f94da71

---
+++ HGInetMiFacturaElectronicaWeb/Views/Demos/Demos.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HGInetMiFacturaElectronicaWeb.Demos
+{
+    public partial class Demos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
