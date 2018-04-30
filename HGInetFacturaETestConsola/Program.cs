cd66f01bdabb7fff430fca50e6f857e397c3cd60
Atamayo <atamayo.hgi@gmail.com>
2018-04-30 21:12:50 +0000

    se elimina la siguiente línea:
    string fecha_t = fecha.ToString(Fecha.formato_fecha_javaT);
    
    
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@170 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetFacturaETestConsola/Program.cs
+++ HGInetFacturaETestConsola/Program.cs
@@ -10,116 +10,114 @@ using System.Text.RegularExpressions;
 
 namespace HGInetFacturaETestConsola
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			try
-			{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
 
 
-				DateTime fecha = DateTime.Now;
+                DateTime fecha = DateTime.Now;
 
-				string fecha_t = fecha.ToString(Fecha.formato_fecha_javaT);
+                string url_ws_consulta = "http://facturaelectronica.dian.gov.co/habilitacion/B2BIntegrationEngine/FacturaElectronica/consultaDocumentos.wsdl";
+                string id_software = "606f5740-c6b9-494f-931c-5a6b3e22d72c";
+                string clave = "Prueba2018";
+                string nit_facturador = "811021438";
+                string cufe = "c25cbb4eef8b3135375756347153855d52dd389e";
+                string prefijo = "";
+                string documento = "990000381";
+                DateTime fecha_documento = new DateTime(2018, 4, 30);
 
-				string url_ws_consulta = "http://facturaelectronica.dian.gov.co/habilitacion/B2BIntegrationEngine/FacturaElectronica/consultaDocumentos.wsdl";
-				string id_software = "606f5740-c6b9-494f-931c-5a6b3e22d72c";
-				string clave = "Prueba2018";
-				string nit_facturador = "811021438";
-				string cufe = "c25cbb4eef8b3135375756347153855d52dd389e";
-				string prefijo = "";
-				string documento = "990000381";
-				DateTime fecha_documento = new DateTime(2018, 4, 30);
+                Ctl_ConsultaTransacciones.Consultar(Guid.NewGuid(), id_software, clave, 1, prefijo, documento, nit_facturador, fecha_documento, cufe, url_ws_consulta, Directorio.ObtenerDirectorioRaiz());
 
-				Ctl_ConsultaTransacciones.Consultar(Guid.NewGuid(), id_software, clave, 1, prefijo, documento, nit_facturador, fecha_documento, cufe, url_ws_consulta, Directorio.ObtenerDirectorioRaiz());
+                //PruebaCufe();
 
-				//PruebaCufe();
+            }
+            catch (Exception excepcion)
+            {
+                System.Diagnostics.Debug.WriteLine("ERROR: " + excepcion.Message);
+            }
 
-			}
-			catch (Exception excepcion)
-			{
-				System.Diagnostics.Debug.WriteLine("ERROR: " + excepcion.Message);
-			}
+        }
 
-		}
+        public static void PruebaCufe()
+        {
+            try
+            {
 
-		public static void PruebaCufe()
-		{
-			try
-			{
+                string cufe = HGInetFacturaEServicios.Ctl_Factura.CalcularCUFE("dd85db55545bd6566f36b0fd3be9fd8555c36e", "990000402", new DateTime(2018, 4, 23), "811021438", "13", "1020395355", 47500.00M, 39916.00M, 7584.00M, 0.00M, 0.00M);
 
-				string cufe = HGInetFacturaEServicios.Ctl_Factura.CalcularCUFE("dd85db55545bd6566f36b0fd3be9fd8555c36e", "990000402", new DateTime(2018, 4, 23), "811021438", "13", "1020395355", 47500.00M, 39916.00M, 7584.00M, 0.00M, 0.00M);
-
-				System.Diagnostics.Debug.WriteLine("CUFE GENERADO : " + cufe);
+                System.Diagnostics.Debug.WriteLine("CUFE GENERADO : " + cufe);
 
 
-				string cufeNC = Ctl_NotaCredito.CalcularCUFE("5790af0483698d226240b7c4abafd49eb0dc8d480f6a7e44dfe4a523fed2129a", "3d03ae717f28dab49c7588ffa4ef006f776ac468", "602", new DateTime(2018, 4, 23), "811021438", "13", "79758318", 687196.00M, 607870.00M, 109720.00M, 0.00M, 0.00M);
+                string cufeNC = Ctl_NotaCredito.CalcularCUFE("5790af0483698d226240b7c4abafd49eb0dc8d480f6a7e44dfe4a523fed2129a", "3d03ae717f28dab49c7588ffa4ef006f776ac468", "602", new DateTime(2018, 4, 23), "811021438", "13", "79758318", 687196.00M, 607870.00M, 109720.00M, 0.00M, 0.00M);
 
-				System.Diagnostics.Debug.WriteLine("cufeNC GENERADO : " + cufeNC);
+                System.Diagnostics.Debug.WriteLine("cufeNC GENERADO : " + cufeNC);
 
-			}
-			catch (Exception excepcion)
-			{
-				System.Diagnostics.Debug.WriteLine("ERRO GENERANDO CUFE" + excepcion.Message);
-			}
+            }
+            catch (Exception excepcion)
+            {
+                System.Diagnostics.Debug.WriteLine("ERRO GENERANDO CUFE" + excepcion.Message);
+            }
 
-		}
-
-
-		public static void test1()
-		{
+        }
 
 
-			//HGInetFacturaEServicios.ServicioFactura.Factura factura = new HGInetFacturaEServicios.ServicioFactura.Factura();
-
-			HGInetFacturaEServicios.Ctl_Factura.Test("http://habilitacion.mifacturaenlinea.com.co");
-
-
-			Regex isnumber = new Regex(@"^(0|([1-9][0-9]*))(\.\d\d$)$");
-
-			//  ^[0-9]+(\\,[0-9]{2,2})?$
-
-			//  ^[0-9]([.,][0-9]{1,3})?$
-
-			//  [0-9]+(\.[0-9][0-9]?)?
-
-			//  ^\d+(\.\d{1,2})?$
-
-			// @"\d{1,12}\.\d\d$"
-
-			//  @"(^(0|([1-9][0-9]*))(\.[0-9]{1,2})?$)|(^(0{0,1}|([1-9][0-9]*))(\.[0-9]{2,2})?$)"
-
-			decimal valor = 102.3m;
-			string valor_txt = Convert.ToString(valor).Replace(",", ".");
-
-			bool decimal1 = isnumber.IsMatch(valor_txt);
+        public static void test1()
+        {
 
 
-			valor = 2365478523102.30m;
-			valor_txt = Convert.ToString(valor).Replace(",", ".");
+            //HGInetFacturaEServicios.ServicioFactura.Factura factura = new HGInetFacturaEServicios.ServicioFactura.Factura();
 
-			bool decimal2 = isnumber.IsMatch(valor_txt);
-
+            HGInetFacturaEServicios.Ctl_Factura.Test("http://habilitacion.mifacturaenlinea.com.co");
 
 
-			valor = 102.307m;
-			valor_txt = Convert.ToString(valor).Replace(",", ".");
+            Regex isnumber = new Regex(@"^(0|([1-9][0-9]*))(\.\d\d$)$");
 
-			bool decimal3 = isnumber.IsMatch(valor_txt);
+            //  ^[0-9]+(\\,[0-9]{2,2})?$
+
+            //  ^[0-9]([.,][0-9]{1,3})?$
+
+            //  [0-9]+(\.[0-9][0-9]?)?
+
+            //  ^\d+(\.\d{1,2})?$
+
+            // @"\d{1,12}\.\d\d$"
+
+            //  @"(^(0|([1-9][0-9]*))(\.[0-9]{1,2})?$)|(^(0{0,1}|([1-9][0-9]*))(\.[0-9]{2,2})?$)"
+
+            decimal valor = 102.3m;
+            string valor_txt = Convert.ToString(valor).Replace(",", ".");
+
+            bool decimal1 = isnumber.IsMatch(valor_txt);
 
 
-			valor = 0.30m;
-			valor_txt = Convert.ToString(valor).Replace(",", ".");
+            valor = 2365478523102.30m;
+            valor_txt = Convert.ToString(valor).Replace(",", ".");
 
-			bool decimal4 = isnumber.IsMatch(valor_txt);
+            bool decimal2 = isnumber.IsMatch(valor_txt);
 
 
 
-			decimal x = 0.00m;
+            valor = 102.307m;
+            valor_txt = Convert.ToString(valor).Replace(",", ".");
 
-			decimal y = 12;
+            bool decimal3 = isnumber.IsMatch(valor_txt);
 
-			x = y;
-		}
-	}
+
+            valor = 0.30m;
+            valor_txt = Convert.ToString(valor).Replace(",", ".");
+
+            bool decimal4 = isnumber.IsMatch(valor_txt);
+
+
+
+            decimal x = 0.00m;
+
+            decimal y = 12;
+
+            x = y;
+        }
+    }
 }
