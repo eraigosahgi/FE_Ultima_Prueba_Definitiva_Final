abf629cc797af44c67d70b1131a3ba454da7af44
Jflores <jflores.hgi@gmail.com>
2018-05-08 17:00:46 +0000

    Se cambia la forma de llenar el grid devexpress por datagrid Jquery
    Se cambia la lista de destados de la dian
    
    Se crea un metodo para consultar desde que inicia la vista
    
    
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@238 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectronicaWeb/Scripts/Pages/DocumentosObligado.js
+++ HGInetMiFacturaElectronicaWeb/Scripts/Pages/DocumentosObligado.js
@@ -20,6 +20,7 @@ DocObligadoApp.controller('DocObligadoController', function DocObligadoControlle
     	console.log(response.data[0]);
 
     	codigo_facturador = response.data[0].Identificacion;
+    	consultar();
     });
 
 
@@ -55,8 +56,8 @@ DocObligadoApp.controller('DocObligadoController', function DocObligadoControlle
                 Enabled: true,
                 placeholder: "Seleccione un Item",
                 onValueChanged: function (data) {
-                    console.log("EstadoDian", data.value.id);
-                    estado_dian = data.value.id;
+                    console.log("EstadoDian", data.value.ID);
+                    estado_dian = data.value.ID;
                 }
             },
             EstadoRecibo: {
@@ -70,8 +71,8 @@ DocObligadoApp.controller('DocObligadoController', function DocObligadoControlle
                 Enabled: true,
                 placeholder: "Seleccione un Item",
                 onValueChanged: function (data) {
-                    console.log("EstadoRecibo", data.value.id);
-                    estado_recibo = data.value.id;
+                    console.log("EstadoRecibo", data.value.ID);
+                    estado_recibo = data.value.ID;
                 }
             },
             NumeroDocumento: {
@@ -96,109 +97,114 @@ DocObligadoApp.controller('DocObligadoController', function DocObligadoControlle
         text: 'Consultar',
         type: 'default',
         onClick: function (e) {
-
-            console.log("Ingresó al evento del botón");
-
-            if (fecha_inicio == "")
-                fecha_inicio = now.toISOString();
-
-            if (fecha_fin == "")
-                fecha_fin = now.toISOString();
-
-            console.log("FILTROS DE BÚSQUEDA:\n" + "codigo_adquiriente:", codigo_adquiriente, "\nnumero_documento:", numero_documento, "\nestado_recibo:", estado_recibo, "\nfecha_inicio:", fecha_inicio, "\nfecha_fin:", fecha_fin);
-
-            //Obtiene los datos del web api
-            //ControladorApi: /Api/Documentos/
-            //Datos GET: codigo_facturador, numero_documento, codigo_adquiriente, estado_dian, estado_recibo, fecha_inicio, fecha_fin
-            $http.get('/api/Documentos?codigo_facturador=' + codigo_facturador + '&numero_documento=' + numero_documento + '&codigo_adquiriente=' + codigo_adquiriente + '&estado_dian=' + estado_dian + '&estado_recibo=' + estado_recibo + '&fecha_inicio=' + fecha_inicio + '&fecha_fin=' + fecha_fin).then(function (response) {
-
-                console.log("Ingresó a cargar la data.");
-
-                $scope.dataGridOptions = {
-                    dataSource: response.data,
-                    paging: {
-                        pageSize: 20
-                    },
-                    pager: {
-                        showPageSizeSelector: true,
-                        allowedPageSizes: [5, 10, 20],
-                        showInfo: true
-                    },
-                    columns: [
-                        {
-                            caption: "Archivos",
-                            cellTemplate: function (container, options) {
-                                $("<div>")
-                                    .append($("<a target='_blank' class='icon-file-pdf' href='" + options.data.Pdf + "'>&nbsp;&nbsp;<a target='_blank' class='icon-file-xml' href='" + options.data.Xml + "'>&nbsp;&nbsp;<a target='_blank' class='icon-mail-read' href='" + "#" + "'>"))
-                                    .append($(""))
-                                    .appendTo(container);
-                            }
-                        },
-                        {
-                            caption: "Documento",
-                            dataField: "NumeroDocumento",
-                        },
-                        {
-                            caption: "Fecha Documento",
-                            dataField: "DatFechaDocumento",
-                            dataType: "date",
-                            validationRules: [{
-                                type: "required",
-                                message: "El campo Fecha es obligatorio."
-                            }]
-                        },
-                        {
-                            caption: "Fecha Vencimiento",
-                            dataField: "DatFechaVencDocumento",
-                            dataType: "date",
-                            validationRules: [{
-                                type: "required",
-                                message: "El campo Fecha es obligatorio."
-                            }]
-                        },
-                        {
-                            caption: "Valor Total",
-                            dataField: "IntVlrTotal"
-                        },
-                         {
-                             caption: "Identificación Adquiriente",
-                             dataField: "IdentificacionAdquiriente"
-                         },
-                          {
-                              caption: "Nombre Adquiriente",
-                              dataField: "NombreAdquiriente"
-                          },
-                          {
-                              caption: "Estado",
-                              dataField: "EstadoFactura",
-                          },
-                          {
-                              caption: "Estado Acuse",
-                              dataField: "EstadoAcuse",
-                          },
-                          {
-                              caption: "Motivo Rechazo",
-                              dataField: "MotivoRechazo",
-                          },                          
-						  {
-                              dataField: "",
-                              cellTemplate: function (container, options) {
-                                  $("<div>")
-                                      .append($("<a target='_blank' class='icon-check' href='" + options.data.RutaAcuse + "'>Acuse</a>"))
-                                      .appendTo(container);
-                              }
-                          }
-                    ],
-                    filterRow: {
-                        visible: true
-                    },
-                };
-                console.log("DATOS DE RETORNO DE WEB API", response.data);
-
-                console.log("Salió del método");
-            });
+            consultar();
         }
     };
+
+    function consultar() {
+
+        console.log("Ingresó al evento del botón");
+
+        if (fecha_inicio == "")
+            fecha_inicio = now.toISOString();
+
+        if (fecha_fin == "")
+            fecha_fin = now.toISOString();
+
+        console.log("FILTROS DE BÚSQUEDA:\n" + "codigo_adquiriente:", codigo_adquiriente, "\nnumero_documento:", numero_documento, "\nestado_recibo:", estado_recibo, "\nfecha_inicio:", fecha_inicio, "\nfecha_fin:", fecha_fin);
+
+        //Obtiene los datos del web api
+        //ControladorApi: /Api/Documentos/
+        //Datos GET: codigo_facturador, numero_documento, codigo_adquiriente, estado_dian, estado_recibo, fecha_inicio, fecha_fin
+        $http.get('/api/Documentos?codigo_facturador=' + codigo_facturador + '&numero_documento=' + numero_documento + '&codigo_adquiriente=' + codigo_adquiriente + '&estado_dian=' + estado_dian + '&estado_recibo=' + estado_recibo + '&fecha_inicio=' + fecha_inicio + '&fecha_fin=' + fecha_fin).then(function (response) {
+
+            console.log("Ingresó a cargar la data.");
+            console.log("Datossss", response.data);
+            $("#gridDocumentos").dxDataGrid({
+                dataSource: response.data,
+                paging: {
+                    pageSize: 20
+                },
+                pager: {
+                    showPageSizeSelector: true,
+                    allowedPageSizes: [5, 10, 20],
+                    showInfo: true
+                },
+                columns: [
+                    {
+                        caption: "Archivos",
+                        cellTemplate: function (container, options) {
+                            $("<div>")
+                                .append($("<a target='_blank' class='icon-file-pdf' href='" + options.data.Pdf + "'>&nbsp;&nbsp;<a target='_blank' class='icon-file-xml' href='" + options.data.Xml + "'>&nbsp;&nbsp;<a target='_blank' class='icon-mail-read' href='" + "#" + "'>"))
+                                .append($(""))
+                                .appendTo(container);
+                        }
+                    },
+                    {
+                        caption: "Documento",
+                        dataField: "NumeroDocumento",
+                    },
+                    {
+                        caption: "Fecha Documento",
+                        dataField: "DatFechaDocumento",
+                        dataType: "date",
+                        validationRules: [{
+                            type: "required",
+                            message: "El campo Fecha es obligatorio."
+                        }]
+                    },
+                    {
+                        caption: "Fecha Vencimiento",
+                        dataField: "DatFechaVencDocumento",
+                        dataType: "date",
+                        validationRules: [{
+                            type: "required",
+                            message: "El campo Fecha es obligatorio."
+                        }]
+                    },
+                    {
+                        caption: "Valor Total",
+                        dataField: "IntVlrTotal"
+                    },
+                     {
+                         caption: "Identificación Adquiriente",
+                         dataField: "IdentificacionAdquiriente"
+                     },
+                      {
+                          caption: "Nombre Adquiriente",
+                          dataField: "NombreAdquiriente"
+                      },
+                      {
+                          caption: "Estado",
+                          dataField: "EstadoFactura",
+                      },
+                      {
+                          caption: "Estado Acuse",
+                          dataField: "EstadoAcuse",
+                      },
+                      {
+                          caption: "Motivo Rechazo",
+                          dataField: "MotivoRechazo",
+                      },
+                      {
+                          dataField: "",
+                          cellTemplate: function (container, options) {
+                              $("<div>")
+                                  .append($("<a target='_blank' class='icon-check' href='" + options.data.RutaAcuse + "'>Acuse</a>"))
+                                  .appendTo(container);
+                          }
+                      }
+                ],
+                filterRow: {
+                    visible: true
+                },
+            });
+            console.log("DATOS DE RETORNO DE WEB API", response.data);
+
+            console.log("Salió del método");
+        });
+
+    }
 
 
 });
@@ -215,7 +221,15 @@ var items_recibo =
 var items_dian =
     [
     { ID: "*", Texto: 'Obtener Todos' },
-    { ID: "0", Texto: 'Pendiente' },
-    { ID: "1", Texto: 'Aprobado' },
-    { ID: "2", Texto: 'Rechazado' }
+    { ID: "1", Texto: 'Recepción' },
+    { ID: "2", Texto: 'Validación Documento' },
+    { ID: "3", Texto: 'Generación UBL' },
+    { ID: "4", Texto: 'Almacenamiento XML' },
+    { ID: "5", Texto: 'Firma XML' },
+    { ID: "6", Texto: 'Compresión XML' },
+    { ID: "7", Texto: 'Envío ZIP' },
+    { ID: "8", Texto: 'Envío E-mail Adquiriente' },
+    { ID: "9", Texto: 'Recepción Acuse' },
+    { ID: "10", Texto: 'Envío E-mail Acuse' },
+    { ID: "11", Texto: 'Fin Proceso' }
     ];
