b55b4b1379214ae9cd339d0518037ca2715b3bff
Mparamo <mparamo@noemail.com>
2018-05-08 03:00:44 +0000

    - Se crea el controlador WebApi para obtener la sesión
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@214 30a10932-8c21-4e4d-906a-15261f94da71

---
+++ HGInetMiFacturaElectronicaWeb/Controllers/Services/DatosSesionController.cs
@@ -0,0 +1,59 @@
+﻿using HGInetMiFacturaElectonicaData.Modelo;
+using HGInetMiFacturaElectronicaWeb.Seguridad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HGInetMiFacturaElectronicaWeb.Controllers.Services
+{
+    public class DatosSesionController : ApiController
+    {
+
+        public IHttpActionResult Get()
+        {
+            List<TblEmpresas> datos = new List<TblEmpresas>();
+
+            datos.Add(Sesion.DatosEmpresa);
+
+            var retorno = datos.Select(d => new
+            {
+                FechaActualizacion = d.DatFechaActualizacion,
+                FechaIngreso = d.DatFechaIngreso,
+                Adquiriente = d.IntAdquiriente,
+                Habilitacion = d.IntHabilitacion,
+                Id = d.IntId,
+                IdentificacionDV = d.IntIdentificacionDv,
+                Obligado = d.IntObligado,
+                Identificacion = d.StrIdentificacion,
+                IdSeguridad = d.StrIdSeguridad,
+                Mail = d.StrMail,
+                Observaciones = d.StrObservaciones,
+                RazonSocial = d.StrRazonSocial,
+                Serial = d.StrSerial,
+                TipoIdentificacion = d.StrTipoIdentificacion
+            });
+
+            return Ok(retorno);
+        }
+
+
+        public dynamic ConvertirEmpresa(List<TblEmpresas> empresa)
+        {
+
+            var datos = "";
+
+            var retorno = datos.Select(d => new
+            {
+
+
+            });
+
+
+            return retorno;
+        }
+
+    }
+}
