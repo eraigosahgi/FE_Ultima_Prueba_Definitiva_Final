ffb806d56a7f4116df36032182c3da259812c9c1
Mparamo <mparamo@noemail.com>
2018-05-08 03:00:24 +0000

    - Se crean las clases controladoras de la sesión para Web Api - Se crea el controlador WebApi para obtener la sesión
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@213 30a10932-8c21-4e4d-906a-15261f94da71

---
+++ HGInetMiFacturaElectronicaWeb/Seguridad/Sesion.cs
@@ -0,0 +1,108 @@
+﻿using HGInetMiFacturaElectonicaController.Configuracion;
+using HGInetMiFacturaElectonicaData.Modelo;
+using System;
+using System.Web;
+using System.Web.Http;
+
+namespace HGInetMiFacturaElectronicaWeb.Seguridad
+{
+    public class Sesion
+    {
+        #region Propiedades
+
+        /// <summary>
+        /// Datos de la empresa 
+        /// </summary>
+        public static TblEmpresas DatosEmpresa
+        {
+            get
+            {
+                ValidarSesion();
+                if (HttpContext.Current.Session["datos_empresa"] == null)
+                {
+                    TblEmpresas empresa = new TblEmpresas();
+                    HttpContext.Current.Session["datos_empresa"] = empresa;
+                }
+                return (TblEmpresas)HttpContext.Current.Session["datos_empresa"];
+            }
+        }
+
+        /// <summary>
+        /// Datos del usuario
+        /// </summary>
+        public static TblUsuarios DatosUsuario
+        {
+            get
+            {
+                ValidarSesion();
+                if (HttpContext.Current.Session["datos_usuario"] == null)
+                {
+                    TblUsuarios usuario = new TblUsuarios();
+                    HttpContext.Current.Session["datos_usuario"] = usuario;
+                }
+                return (TblUsuarios)HttpContext.Current.Session["datos_usuario"];
+            }
+        }
+
+
+        public static string Token
+        {
+            get
+            {
+                ValidarSesion();
+                if (HttpContext.Current.Session["token"] == null)
+                {
+                    HttpContext.Current.Session["token"] = string.Empty;
+                }
+                return HttpContext.Current.Session["token"].ToString();
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Valida que se encuentren los datos necesarios en la sesión
+        /// </summary>
+        public static void ValidarSesion()
+        {
+            HttpContext context = HttpContext.Current;
+
+            if (context.Session == null)
+                throw new ApplicationException("No se encontraron datos en la sesión; ingrese nuevamente.");
+
+
+            if (context.Session["datos_empresa"] == null || context.Session["datos_usuario"] == null)
+                throw new ApplicationException("No se encontraron los datos de autenticación en la sesión; ingrese nuevamente.");
+        }
+
+        /// <summary>
+        /// Guarda los datos del usuario en la sesión web
+        /// </summary>
+        /// <returns>datos de autenticacion</returns>
+        public void GuardarSesionWeb(System.Guid id_seguridad)
+        {
+            try
+            {
+                //Obtiene los datos del usuario autenticado
+                Ctl_Usuario clase_usuario = new Ctl_Usuario();
+                TblUsuarios datos_usuario = clase_usuario.ObtenerIdSeguridad(id_seguridad);
+
+                if (datos_usuario != null)
+                    HttpContext.Current.Session.Add("datos_usuario", datos_usuario);
+
+                //Obtiene la información de la empresa autenticada.
+                Ctl_Empresa clase_empresa = new Ctl_Empresa();
+                TblEmpresas datos_empresa = clase_empresa.ObtenerId(datos_usuario.IntIdEmpresa);
+
+                if (datos_empresa != null)
+                    HttpContext.Current.Session.Add("datos_empresa", datos_empresa);
+
+            }
+            catch (Exception excepcion)
+            {
+                throw new ApplicationException(string.Format("{0}{1}{2}", "Error al construir la sesíón del usuario con los datos de autenticación. ", excepcion.Message, excepcion.InnerException));
+            }
+        }
+
+    }
+}
