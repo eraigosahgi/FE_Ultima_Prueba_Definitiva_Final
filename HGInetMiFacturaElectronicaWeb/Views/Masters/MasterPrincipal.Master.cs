5aa292e7ff4c396f60ed7cd3731eec36c7dda7e9
Mparamo <mparamo@noemail.com>
2018-05-08 02:59:16 +0000

    Se cargan los datos de la empresa y del usuario en la sesión para mostrarlos
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@212 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectronicaWeb/Views/Masters/MasterPrincipal.Master.cs
+++ HGInetMiFacturaElectronicaWeb/Views/Masters/MasterPrincipal.Master.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HGInetMiFacturaElectonicaData.Modelo;
+using HGInetMiFacturaElectronicaWeb.Seguridad;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,34 @@ namespace HGInetMiFacturaElectronicaWeb.Views.Masters
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // obtiene los datos de la sesión para mostrarlos
+            if (Sesion.DatosEmpresa != null && Sesion.DatosUsuario != null)
+            {
+                // carga los datos de la empresa en la página master
+                TblEmpresas datos_empresa = Sesion.DatosEmpresa;
+                LblNombreEmpresa.InnerText = datos_empresa.StrRazonSocial;
+                
+                // carga los datos del usuario en la página master
+                TblUsuarios datos_usuario = Sesion.DatosUsuario;
+                LblCodigoUsuario.InnerText = datos_usuario.StrUsuario;
+                LblNombreUsuarioDet.InnerText = string.Format("{0} {1}", datos_usuario.StrNombres, datos_usuario.StrApellidos);
+                lblEmailUsuario.InnerText = datos_usuario.StrMail;
+                LblNombreUsuario.InnerText = string.Format("{0} {1}", datos_usuario.StrNombres, datos_usuario.StrApellidos);
+            }
+        }
 
+        /// <summary>
+        /// Evento al presionar el link para cerrar la sesión
+        /// </summary>
+        protected void LinkCerrarSesion_Click(object sender, EventArgs e)
+        {
+            if (Session != null)
+            {
+                Session.Clear();
+                Session.Abandon();
+            }
+
+            Response.Redirect("~/Views/Login/Default.aspx");
         }
     }
 }
--- HGInetMiFacturaElectronicaWeb/Views/Masters/MasterPrincipal.Master.designer.cs
+++ HGInetMiFacturaElectronicaWeb/Views/Masters/MasterPrincipal.Master.designer.cs
@@ -40,6 +40,33 @@ namespace HGInetMiFacturaElectronicaWeb.Views.Masters {
         protected global::System.Web.UI.WebControls.Label Label_version_actualizacion;
         
         /// <summary>
+        /// Control LblNombreUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl LblNombreUsuario;
+        
+        /// <summary>
+        /// Control LblCodigoUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl LblCodigoUsuario;
+        
+        /// <summary>
+        /// Control LblNombreUsuarioDet.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl LblNombreUsuarioDet;
+        
+        /// <summary>
         /// Control EmailUsuario.
         /// </summary>
         /// <remarks>
@@ -49,13 +76,40 @@ namespace HGInetMiFacturaElectronicaWeb.Views.Masters {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl EmailUsuario;
         
         /// <summary>
-        /// Control icono_email.
+        /// Control lblEmailUsuario.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl icono_email;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl lblEmailUsuario;
+        
+        /// <summary>
+        /// Control LinkCerrarSesionHeader.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkCerrarSesionHeader;
+        
+        /// <summary>
+        /// Control LblNombreEmpresa.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl LblNombreEmpresa;
+        
+        /// <summary>
+        /// Control LinkCerrarSesion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkCerrarSesion;
         
         /// <summary>
         /// Control ContenidoPagina.
