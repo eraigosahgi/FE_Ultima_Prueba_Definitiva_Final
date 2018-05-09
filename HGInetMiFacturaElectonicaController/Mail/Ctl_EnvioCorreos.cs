c571b19541051554ad51655603c604eafc48c3fb
Atamayo <atamayo.hgi@gmail.com>
2018-05-09 14:56:57 +0000

    ajustes metodo NotificacionDocumento: se elimina parametro estado y se modifica por nuevo_email para reenvio. se valida dato de parametro telefono.
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@247 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectonicaController/Mail/Ctl_EnvioCorreos.cs
+++ HGInetMiFacturaElectonicaController/Mail/Ctl_EnvioCorreos.cs
@@ -240,23 +240,23 @@ namespace HGInetMiFacturaElectonicaController
 
 		}
 
-		/// <summary>
-		/// Envía mail al Adquiriente con los archivos generados
-		/// </summary>
-		/// <param name="documento">Datos del documento enviado</param>
-		/// <param name="archivos">archivos de envio</param>
-		/// <param name="obj_respuesta">datos del objeto de respuesta</param>
-		/// <returns></returns>
-		public bool NotificacionDocumento(TblDocumentos documento, string telefono, string estado_doc)
-		{
+        /// <summary>
+        /// Envía mail al Adquiriente con los archivos generados
+        /// </summary>
+        /// <param name="documento"></param>
+        /// <param name="telefono"></param>
+        /// <param name="nuevo_email">indica el e-mail del destinatario (si es igual a null, se envía a el email de la empresa del adquiriente)</param>
+        /// <returns></returns>
+        public bool NotificacionDocumento(TblDocumentos documento, string telefono, string nuevo_email = "")
+        {
 
-			try
-			{
-				PlataformaData plataforma = HgiConfiguracion.GetConfiguration().PlataformaData;
+            try
+            {
+                PlataformaData plataforma = HgiConfiguracion.GetConfiguration().PlataformaData;
 
-				string ruta_plantilla_html = string.Format("{0}{1}", Directorio.ObtenerDirectorioRaiz(), Constantes.RutaPlantillaHtmlDocumentos);
+                string ruta_plantilla_html = string.Format("{0}{1}", Directorio.ObtenerDirectorioRaiz(), Constantes.RutaPlantillaHtmlDocumentos);
 
-				string asunto = "Factura Electrónica";
+                string asunto = "Factura Electrónica";
 
                 // obtiene los datos del facturador electrónico
                 Ctl_Empresa facturador_electronico = new Ctl_Empresa();
@@ -270,15 +270,19 @@ namespace HGInetMiFacturaElectonicaController
 
                 // obtiene los datos del adquiriente
                 Ctl_Empresa adquiriente = new Ctl_Empresa();
-				TblEmpresas empresa_adquiriente = adquiriente.ObtenerId(documento.IntIdEmpresaAdquiriente);
+                TblEmpresas empresa_adquiriente = adquiriente.ObtenerId(documento.IntIdEmpresaAdquiriente);
 
                 // recibe el email el adquiriente
-				DestinatarioEmail destinatario = new DestinatarioEmail();
-				destinatario.Nombre = empresa_adquiriente.StrRazonSocial;
-				destinatario.Email = empresa_adquiriente.StrMail;
+                DestinatarioEmail destinatario = new DestinatarioEmail();
+                destinatario.Nombre = empresa_adquiriente.StrRazonSocial;
 
-				List<DestinatarioEmail> correos_destino = new List<DestinatarioEmail>();
-				correos_destino.Add(destinatario);
+                if (string.IsNullOrWhiteSpace(nuevo_email))
+                    destinatario.Email = empresa_adquiriente.StrMail;
+                else
+                    destinatario.Email = nuevo_email;
+
+                List<DestinatarioEmail> correos_destino = new List<DestinatarioEmail>();
+                correos_destino.Add(destinatario);
 
 
                 // envía correo electrónico con copia de auditoría
@@ -293,68 +297,68 @@ namespace HGInetMiFacturaElectonicaController
                     correos_copia_oculta.Add(copia_oculta);
                 }
 
-                
-				// plantilla Html
-				if (!string.IsNullOrWhiteSpace(ruta_plantilla_html))
-				{
-					FileInfo file = new FileInfo(ruta_plantilla_html);
 
-					string mensaje = file.OpenText().ReadToEnd();
+                // plantilla Html
+                if (!string.IsNullOrWhiteSpace(ruta_plantilla_html))
+                {
+                    FileInfo file = new FileInfo(ruta_plantilla_html);
 
-
-					if (file != null)
-					{
-						// Datos Facturador Electronico
-						mensaje = mensaje.Replace("{ImagenLogo}", "<img id='ImgLogo' src='" + "" + "' style='border: none; border-radius: 0px; display: block; outline: none; text-decoration: none; width: 100%; height: auto;' width='233' />");
-						mensaje = mensaje.Replace("{NombreFacturador}", empresa_obligado.StrRazonSocial);
-						mensaje = mensaje.Replace("{NitFacturador}", empresa_obligado.StrIdentificacion);
-						mensaje = mensaje.Replace("{EmailFacturador}", empresa_obligado.StrMail);
-						mensaje = mensaje.Replace("{TelefonoFacturador}", telefono);
-
-						// Datos del Tercero
-						mensaje = mensaje.Replace("{NombreTercero}", empresa_adquiriente.StrRazonSocial);
-						mensaje = mensaje.Replace("{NitTercero}", empresa_adquiriente.StrIdentificacion);
-						mensaje = mensaje.Replace("{Digitov}", empresa_adquiriente.IntIdentificacionDv.ToString());
-						mensaje = mensaje.Replace("{CorreoTercero}", empresa_adquiriente.StrMail);
-						mensaje = mensaje.Replace("{GuidDocumento}", documento.StrIdSeguridad.ToString());
+                    string mensaje = file.OpenText().ReadToEnd();
 
 
-						//Datos del Documento
-						string titulo_factura = string.Empty;
-
-						switch (documento.IntDocTipo)
-						{
-							case 1:
-								titulo_factura = "Factura";
-								break;
-							case 2:
-								titulo_factura = "Nota Dédito";
-								break;
-							case 3:
-								titulo_factura = "Nota Crédito";
-								break;
-						}
-
-						string estado_factura = string.Empty;
-
-						if (estado_doc.Equals("7200002"))
-						{
-							estado_factura = "Entregado DIAN";
-						}
-
-						mensaje = mensaje.Replace("{TipoDocumento}", titulo_factura);
-						mensaje = mensaje.Replace("{NumeroDocumento}", documento.IntNumero.ToString());
-						mensaje = mensaje.Replace("{FechaDocumento}", documento.DatFechaDocumento.ToString(Fecha.formato_fecha_hginet));
-						mensaje = mensaje.Replace("{TotalDocumento}", String.Format("{0:###,##0.}", documento.IntVlrTotal));
-						mensaje = mensaje.Replace("{EstadoDianDocumento}", estado_factura);
-
-						mensaje = mensaje.Replace("{RutaUrl}", string.Format("{0}{1}", plataforma.RutaPublica, Constantes.PaginaAcuseRecibo.Replace("{id_seguridad}", documento.StrIdSeguridad.ToString())));
+                    if (file != null)
+                    {
+                        // Datos Facturador Electronico
+                        mensaje = mensaje.Replace("{ImagenLogo}", "<img id='ImgLogo' src='" + "" + "' style='border: none; border-radius: 0px; display: block; outline: none; text-decoration: none; width: 100%; height: auto;' width='233' />");
+                        mensaje = mensaje.Replace("{NombreFacturador}", empresa_obligado.StrRazonSocial);
+                        mensaje = mensaje.Replace("{NitFacturador}", empresa_obligado.StrIdentificacion);
+                        mensaje = mensaje.Replace("{EmailFacturador}", empresa_obligado.StrMail);
 
 
-						List<Adjunto> archivos = new List<Adjunto>();
+                        if (!string.IsNullOrWhiteSpace(telefono))
+                            mensaje = mensaje.Replace("{TelefonoFacturador}", string.Format("Teléfono: {0}", telefono));
+                        else
+                            mensaje = mensaje.Replace("{TelefonoFacturador}", "");
 
-						//Archivos para enviar por smtp
-						/*
+                        // Datos del Tercero
+                        mensaje = mensaje.Replace("{NombreTercero}", empresa_adquiriente.StrRazonSocial);
+                        mensaje = mensaje.Replace("{NitTercero}", empresa_adquiriente.StrIdentificacion);
+                        mensaje = mensaje.Replace("{Digitov}", empresa_adquiriente.IntIdentificacionDv.ToString());
+                        mensaje = mensaje.Replace("{CorreoTercero}", empresa_adquiriente.StrMail);
+                        mensaje = mensaje.Replace("{GuidDocumento}", documento.StrIdSeguridad.ToString());
+
+
+                        //Datos del Documento
+                        string titulo_factura = string.Empty;
+
+                        switch (documento.IntDocTipo)
+                        {
+                            case 1:
+                                titulo_factura = "Factura";
+                                break;
+                            case 2:
+                                titulo_factura = "Nota Crédito";
+                                break;
+                            case 3:
+                                titulo_factura = "Nota Débito";
+                                break;
+                        }
+
+                        string estado_factura = "Entregado DIAN";
+
+                        mensaje = mensaje.Replace("{TipoDocumento}", titulo_factura);
+                        mensaje = mensaje.Replace("{NumeroDocumento}", documento.IntNumero.ToString());
+                        mensaje = mensaje.Replace("{FechaDocumento}", documento.DatFechaDocumento.ToString(Fecha.formato_fecha_hginet));
+                        mensaje = mensaje.Replace("{TotalDocumento}", String.Format("{0:###,##0.}", documento.IntVlrTotal));
+                        mensaje = mensaje.Replace("{EstadoDianDocumento}", estado_factura);
+
+                        mensaje = mensaje.Replace("{RutaUrl}", string.Format("{0}{1}", plataforma.RutaPublica, Constantes.PaginaAcuseRecibo.Replace("{id_seguridad}", documento.StrIdSeguridad.ToString())));
+
+
+                        List<Adjunto> archivos = new List<Adjunto>();
+
+                        //Archivos para enviar por smtp
+                        /*
 						if (!string.IsNullOrEmpty(documento.StrUrlArchivoPdf))
 							archivos.Add(documento.StrUrlArchivoPdf);
 
@@ -363,52 +367,52 @@ namespace HGInetMiFacturaElectonicaController
 
 							*/
 
-						byte[] bytes_pdf = Archivo.ObtenerWeb(documento.StrUrlArchivoPdf);
-						string ruta_fisica_pdf = Convert.ToBase64String(bytes_pdf);
-						string nombre_pdf = Path.GetFileName(documento.StrUrlArchivoPdf);
+                        byte[] bytes_pdf = Archivo.ObtenerWeb(documento.StrUrlArchivoPdf);
+                        string ruta_fisica_pdf = Convert.ToBase64String(bytes_pdf);
+                        string nombre_pdf = Path.GetFileName(documento.StrUrlArchivoPdf);
 
 
-						if (!string.IsNullOrEmpty(ruta_fisica_pdf))
-						{
-							Adjunto adjunto = new Adjunto();
-							adjunto.ContenidoB64 = ruta_fisica_pdf;
-							adjunto.Nombre = nombre_pdf;
-							archivos.Add(adjunto);
-						}
+                        if (!string.IsNullOrEmpty(ruta_fisica_pdf))
+                        {
+                            Adjunto adjunto = new Adjunto();
+                            adjunto.ContenidoB64 = ruta_fisica_pdf;
+                            adjunto.Nombre = nombre_pdf;
+                            archivos.Add(adjunto);
+                        }
 
-						byte[] bytes_xml = Archivo.ObtenerWeb(documento.StrUrlArchivoUbl);
-						string ruta_fisica_xml = Convert.ToBase64String(bytes_xml);
-						string nombre_xml = Path.GetFileName(documento.StrUrlArchivoUbl);
+                        byte[] bytes_xml = Archivo.ObtenerWeb(documento.StrUrlArchivoUbl);
+                        string ruta_fisica_xml = Convert.ToBase64String(bytes_xml);
+                        string nombre_xml = Path.GetFileName(documento.StrUrlArchivoUbl);
 
-						if (!string.IsNullOrEmpty(ruta_fisica_xml))
-						{
-							Adjunto adjunto = new Adjunto();
-							adjunto.ContenidoB64 = ruta_fisica_xml;
-							adjunto.Nombre = nombre_xml;
-							archivos.Add(adjunto);
-						}
+                        if (!string.IsNullOrEmpty(ruta_fisica_xml))
+                        {
+                            Adjunto adjunto = new Adjunto();
+                            adjunto.ContenidoB64 = ruta_fisica_xml;
+                            adjunto.Nombre = nombre_xml;
+                            archivos.Add(adjunto);
+                        }
 
 
-						// envía el correo electrónico
-						EnviarEmail(documento.StrIdSeguridad.ToString(), false, mensaje, asunto, true, remitente, correos_destino, null, correos_copia_oculta, "", "", archivos);
+                        // envía el correo electrónico
+                        EnviarEmail(documento.StrIdSeguridad.ToString(), false, mensaje, asunto, true, remitente, correos_destino, null, correos_copia_oculta, "", "", archivos);
 
-					}
-				}
+                    }
+                }
 
-				return true;
-			}
-			catch (Exception excepcion)
-			{
-				throw new ApplicationException(excepcion.Message, excepcion.InnerException);
-			}
-		}
+                return true;
+            }
+            catch (Exception excepcion)
+            {
+                throw new ApplicationException(excepcion.Message, excepcion.InnerException);
+            }
+        }
 
-		/// <summary>
-		/// Envía mail para restablecer contraseña
-		/// </summary>
-		/// <param name="empresa">Datos del Obligado o del Adquiriente</param>
-		/// <param name="usuario">Datos del usuario guardado en BD</param>
-		public void RestablecerClave(TblEmpresas empresa, TblUsuarios usuario)
+        /// <summary>
+        /// Envía mail para restablecer contraseña
+        /// </summary>
+        /// <param name="empresa">Datos del Obligado o del Adquiriente</param>
+        /// <param name="usuario">Datos del usuario guardado en BD</param>
+        public void RestablecerClave(TblEmpresas empresa, TblUsuarios usuario)
 		{
 
 			try
