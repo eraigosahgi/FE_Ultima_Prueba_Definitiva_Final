33173ce35722c45eab61b542be48cdc5e443f0ea
Jflores <jflores.hgi@gmail.com>
2018-05-10 16:45:57 +0000

    Se agrega nuevo archivo para configuracion de formato y control de session
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@256 30a10932-8c21-4e4d-906a-15261f94da71

---
+++ HGInetMiFacturaElectronicaWeb/Scripts/config.js
@@ -0,0 +1,31 @@
+﻿
+var fNumber = {
+    sepMil: ".", // separador para los miles
+    sepDec: ',', // separador para los decimales
+    simbol: "$" || '', //Simbolo de modena
+    formatear: function (num) {
+        num += '';
+        var splitStr = num.split('.');
+        var splitLeft = splitStr[0];
+        var splitRight = splitStr.length > 1 ? this.sepDec + splitStr[1] : '';
+        var regx = /(\d+)(\d{3})/;
+        while (regx.test(splitLeft)) {
+            splitLeft = splitLeft.replace(regx, '$1' + this.sepMil + '$2');
+        }
+        return this.simbol + splitLeft + splitRight;
+    },
+    go: function (num) {
+        return this.formatear(num);
+    }
+}
+
+function control_session(Ruta) {
+    swal({
+        title: "No se encontraron los datos de autenticación en la sesión!",
+        text: "ingrese nuevamente!",
+        icon: "warning",                
+    })
+    .then((willDelete) => {
+        window.location.assign(Ruta);
+    });
+}
