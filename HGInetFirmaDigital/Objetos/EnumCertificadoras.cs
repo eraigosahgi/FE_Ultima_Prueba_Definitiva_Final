49d6819a683332a0ffdedf9fcea4e661fd73f245
Jzea <jzea.hgi@gmail.com>
2018-05-15 15:42:37 +0000

    - Se generan controles de excepciones para seguimiento - Se genera descripción de las empresas certificadoras
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@282 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetFirmaDigital/Objetos/EnumCertificadoras.cs
+++ HGInetFirmaDigital/Objetos/EnumCertificadoras.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,12 +17,14 @@ namespace HGInetFirmaDigital
 		/// GSE- GESTIÓN DE SEGURIDAD ELECTRÓNICA S.A.
 		/// http://www.gse.com.co
 		/// </summary>
+		[Description("GSE")]
 		Gse = 1,
 
 		/// <summary>
 		/// ANDES SCD
 		/// https://www.andesscd.com.co/
 		/// </summary>
+		[Description("Andes")]
 		Andes = 2
 
 	}
