f6d80804578ac29cec4071cf1bd77502934d5408
Mparamo <mparamo@noemail.com>
2018-05-18 04:52:31 +0000

    Se genera un método para probar el almacenamiento del formato pdf desde base64
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@300 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectonicaController/Procesos/Ctl_Formato.cs
+++ HGInetMiFacturaElectonicaController/Procesos/Ctl_Formato.cs
@@ -1,4 +1,4 @@
-﻿using HGInetMiFacturaElectonicaData.Modelo;
+﻿using HGInetMiFacturaElectonicaData.ModeloServicio;
 using LibreriaGlobalHGInet.General;
 using LibreriaGlobalHGInet.Objetos;
 using System;
@@ -10,42 +10,98 @@ using System.Threading.Tasks;
 
 namespace HGInetMiFacturaElectonicaController.Procesos
 {
-	public class Ctl_Formato
-	{
+    public class Ctl_Formato
+    {
 
-		public static string GuardarArchivo(string archivo, FacturaE_Documento documento)
-		{
-			try
-			{
-				string nombre_pdf = string.Empty;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <param name="documento"></param>
+        /// <returns></returns>
+        public static string GuardarArchivo(string archivo, FacturaE_Documento documento)
+        {
+            try
+            {
+                string nombre_pdf = string.Empty;
 
-				if (string.IsNullOrWhiteSpace(archivo))
-					throw new ApplicationException(string.Format("No se encontró información en el archivo. {0}", archivo));
-					
-				string carpeta_pdf = LibreriaGlobalHGInet.Dms.ObtenerCarpetaPrincipal(Directorio.ObtenerDirectorioRaiz(), documento.IdSeguridadTercero.ToString());
-				carpeta_pdf = string.Format(@"{0}{1}", carpeta_pdf, LibreriaGlobalHGInet.Properties.RecursoDms.CarpetaFacturaEDian);
+                if (string.IsNullOrWhiteSpace(archivo))
+                    throw new ApplicationException(string.Format("No se encontró información en el archivo. {0}", archivo));
 
-				// valida la existencia de la carpeta
-				carpeta_pdf = Directorio.CrearDirectorio(carpeta_pdf);
+                string carpeta_pdf = LibreriaGlobalHGInet.Dms.ObtenerCarpetaPrincipal(Directorio.ObtenerDirectorioRaiz(), documento.IdSeguridadTercero.ToString());
+                carpeta_pdf = string.Format(@"{0}{1}", carpeta_pdf, LibreriaGlobalHGInet.Properties.RecursoDms.CarpetaFacturaEDian);
 
-				// ruta del pdf
-				string ruta_pdf = string.Format(@"{0}{1}.pdf", carpeta_pdf, documento.NombreXml);
+                // valida la existencia de la carpeta
+                carpeta_pdf = Directorio.CrearDirectorio(carpeta_pdf);
 
-				//convierte el array de byte en archivo pdf
-				File.WriteAllBytes(ruta_pdf, Convert.FromBase64String(archivo));
+                // ruta del pdf
+                string ruta_pdf = string.Format(@"{0}{1}.pdf", carpeta_pdf, documento.NombreXml);
 
-				nombre_pdf = documento.NombreXml;
+                //convierte el array de byte en archivo pdf
+                File.WriteAllBytes(ruta_pdf, Convert.FromBase64String(archivo));
 
-				return nombre_pdf;
+                nombre_pdf = documento.NombreXml;
 
-			}
-			catch (Exception exec)
-			{
-				throw new ApplicationException(exec.Message);
-			}
+                return nombre_pdf;
+
+            }
+            catch (Exception excepcion)
+            {
+                LogExcepcion.Guardar(excepcion);
+
+                throw excepcion;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <param name="directorio"></param>
+        /// <param name="nombre"></param>
+        /// <returns></returns>
+        public static string GuardarArchivo(Formato archivo, string directorio, string nombre)
+        {
+            try
+            {
+                string nombre_pdf = string.Empty;
+
+                if (archivo == null)
+                    throw new ApplicationException(string.Format("No se encontró información en el archivo."));
+
+                if (string.IsNullOrWhiteSpace(archivo.ArchivoPdf))
+                    throw new ApplicationException(string.Format("No se encontró información en el archivo. {0}", archivo));
+
+                string carpeta_pdf = LibreriaGlobalHGInet.Dms.ObtenerCarpetaPrincipal(Directorio.ObtenerDirectorioRaiz(), directorio);
+                carpeta_pdf = string.Format(@"{0}{1}", carpeta_pdf, LibreriaGlobalHGInet.Properties.RecursoDms.CarpetaFacturaEDian);
+
+                // valida la existencia de la carpeta
+                carpeta_pdf = Directorio.CrearDirectorio(carpeta_pdf);
+
+                // ruta del pdf
+                string ruta_pdf = string.Format(@"{0}{1}.pdf", carpeta_pdf, nombre);
+
+                //convierte el array de byte en archivo pdf
+                File.WriteAllBytes(ruta_pdf, Convert.FromBase64String(archivo.ArchivoPdf));
+
+
+                // url pública del pdf
+                string url_ppal_pdf = LibreriaGlobalHGInet.Dms.ObtenerUrlPrincipal("", directorio);
+
+                nombre_pdf = string.Format(@"{0}{1}/{2}.pdf", url_ppal_pdf, LibreriaGlobalHGInet.Properties.RecursoDms.CarpetaFacturaEDian, nombre);
+
+                return nombre_pdf;
+
+            }
+            catch (Exception excepcion)
+            {
+                LogExcepcion.Guardar(excepcion);
+
+                throw excepcion;
+            }
 
 
 
-		}
-	}
+        }
+    }
 }
