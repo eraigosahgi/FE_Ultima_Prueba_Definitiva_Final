76c7a91dbf81dc2a04450849ffed0993769bc4d3
Jzea <jzea.hgi@gmail.com>
2018-05-19 15:52:19 +0000

    -Se agrega Método para consultar resoluciones en BD y retornar una lista
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@308 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectonicaController/Configuracion/Ctl_EmpresaResolucion.cs
+++ HGInetMiFacturaElectonicaController/Configuracion/Ctl_EmpresaResolucion.cs
@@ -12,144 +12,162 @@ using LibreriaGlobalHGInet.Funciones;
 
 namespace HGInetMiFacturaElectonicaController.Configuracion
 {
-    public class Ctl_EmpresaResolucion : BaseObject<TblEmpresasResoluciones>
-    {
-        #region Constructores 
+	public class Ctl_EmpresaResolucion : BaseObject<TblEmpresasResoluciones>
+	{
+		#region Constructores 
 
-        public Ctl_EmpresaResolucion() : base(new ModeloAutenticacion()) { }
-        public Ctl_EmpresaResolucion(ModeloAutenticacion autenticacion) : base(autenticacion) { }
+		public Ctl_EmpresaResolucion() : base(new ModeloAutenticacion()) { }
+		public Ctl_EmpresaResolucion(ModeloAutenticacion autenticacion) : base(autenticacion) { }
 
-        public Ctl_EmpresaResolucion(string servidor, string basedatos, string usuario, string clave) : base(servidor, basedatos, usuario, clave) { }
-        #endregion
+		public Ctl_EmpresaResolucion(string servidor, string basedatos, string usuario, string clave) : base(servidor, basedatos, usuario, clave) { }
+		#endregion
 
-        /// <summary>
-        /// Obtiene la resolución de una empresa
-        /// </summary>
-        /// <param name="documento_empresa">documento de la empresa</param>
-        /// <param name="numero_resolucion">número de resolución</param>
-        /// <returns>datos de la resolución</returns>
-        public TblEmpresasResoluciones Obtener(string documento_empresa, string numero_resolucion)
-        {
+		/// <summary>
+		/// Obtiene la resolución de una empresa
+		/// </summary>
+		/// <param name="documento_empresa">documento de la empresa</param>
+		/// <param name="numero_resolucion">número de resolución</param>
+		/// <returns>datos de la resolución</returns>
+		public TblEmpresasResoluciones Obtener(string documento_empresa, string numero_resolucion)
+		{
 
-            var datos = (from item in context.TblEmpresasResoluciones
-                         where (item.StrNumResolucion.Equals(numero_resolucion) || numero_resolucion.Equals("*"))
-                         && item.TblEmpresas.StrIdentificacion.Equals(documento_empresa)
-                         select item).FirstOrDefault();
+			var datos = (from item in context.TblEmpresasResoluciones
+						 where (item.StrNumResolucion.Equals(numero_resolucion) || numero_resolucion.Equals("*"))
+						 && item.TblEmpresas.StrIdentificacion.Equals(documento_empresa)
+						 select item).FirstOrDefault();
 
-            return datos;
+			return datos;
 
-        }
+		}
 
-        /// <summary>
-        /// Crea la Resolucion por Empresa en la BD
-        /// </summary>
-        /// <param name="resoluciones">Resoluciones de la empresa</param>
-        /// <returns>Resoluciones guardadas</returns>
-        public TblEmpresasResoluciones Crear(TblEmpresasResoluciones resoluciones)
-        {
-            resoluciones = this.Add(resoluciones);
+		/// <summary>
+		/// Obtiene todas las resoluciónes de una empresa
+		/// </summary>
+		/// <param name="documento_empresa">documento de la empresa</param>
+		/// <param name="numero_resolucion">número de resolución</param>
+		/// <returns>lista de resoluciones</returns>
+		public List<TblEmpresasResoluciones> ObtenerResoluciones(string documento_empresa, string numero_resolucion)
+		{
 
-            return resoluciones;
-        }
+			List<TblEmpresasResoluciones> datos = (from item in context.TblEmpresasResoluciones
+												   where (item.StrNumResolucion.Equals(numero_resolucion) || numero_resolucion.Equals("*"))
+												   && item.TblEmpresas.StrIdentificacion.Equals(documento_empresa)
+												   select item).ToList();
 
-        /// <summary>
-        /// Valida y crea resoluciones
-        /// </summary>
-        /// <param name="resoluciones">Objeto de resoluciones obtenida de la DIAN</param>
-        /// <param name="obligado">Identificacion del facturdor</param>
-        /// <returns>Lista de resoluciones</returns>
-        public List<TblEmpresasResoluciones> Crear(ResolucionesFacturacion resoluciones, string obligado)
-        {
+			return datos;
 
-            Ctl_Empresa empresa = new Ctl_Empresa();
-            TblEmpresas empresaBd = empresa.Obtener(obligado);
+		}
 
-            List<TblEmpresasResoluciones> lista_resolucion = new List<TblEmpresasResoluciones>();
+		/// <summary>
+		/// Crea la Resolucion por Empresa en la BD
+		/// </summary>
+		/// <param name="resoluciones">Resoluciones de la empresa</param>
+		/// <returns>Resoluciones guardadas</returns>
+		public TblEmpresasResoluciones Crear(TblEmpresasResoluciones resoluciones)
+		{
+			resoluciones = this.Add(resoluciones);
 
-            foreach (RangoFacturacion item in resoluciones.RangoFacturacion)
-            {
-                //Valida si la Resolucion ya existe en BD
-                TblEmpresasResoluciones tbl_resolucion_actual = Obtener(empresaBd.StrIdentificacion, item.NumeroResolucion.ToString());
+			return resoluciones;
+		}
 
-                // convierte el objeto del servicio a base de datos
-                TblEmpresasResoluciones tbl_resolucion = Convertir(item, empresaBd);
+		/// <summary>
+		/// Valida y crea resoluciones
+		/// </summary>
+		/// <param name="resoluciones">Objeto de resoluciones obtenida de la DIAN</param>
+		/// <param name="obligado">Identificacion del facturdor</param>
+		/// <returns>Lista de resoluciones</returns>
+		public List<TblEmpresasResoluciones> Crear(ResolucionesFacturacion resoluciones, string obligado)
+		{
 
-                if (tbl_resolucion_actual == null)
-                {
-                    // crea el registro en base de datos
-                    tbl_resolucion = Crear(tbl_resolucion);
+			Ctl_Empresa empresa = new Ctl_Empresa();
+			TblEmpresas empresaBd = empresa.Obtener(obligado);
 
-                    lista_resolucion.Add(tbl_resolucion);
-                }
-                else
-                {
-                    tbl_resolucion_actual.DatFechaVigenciaDesde = tbl_resolucion.DatFechaVigenciaDesde;
-                    tbl_resolucion_actual.DatFechaVigenciaHasta = tbl_resolucion.DatFechaVigenciaHasta;
-                    tbl_resolucion_actual.IntRangoFinal = tbl_resolucion.IntRangoFinal;
-                    tbl_resolucion_actual.IntRangoInicial = tbl_resolucion.IntRangoInicial;
-                    tbl_resolucion_actual.StrClaveTecnica = tbl_resolucion.StrClaveTecnica;
-                    tbl_resolucion_actual.StrPrefijo = tbl_resolucion.StrPrefijo;
-                    tbl_resolucion_actual.DatFechaActualizacion = Fecha.GetFecha();
+			List<TblEmpresasResoluciones> lista_resolucion = new List<TblEmpresasResoluciones>();
 
-                    this.Edit(tbl_resolucion_actual);
-                    lista_resolucion.Add(tbl_resolucion_actual);
-                }
-            }
+			foreach (RangoFacturacion item in resoluciones.RangoFacturacion)
+			{
+				//Valida si la Resolucion ya existe en BD
+				TblEmpresasResoluciones tbl_resolucion_actual = Obtener(empresaBd.StrIdentificacion, item.NumeroResolucion.ToString());
 
-            return lista_resolucion;
+				// convierte el objeto del servicio a base de datos
+				TblEmpresasResoluciones tbl_resolucion = Convertir(item, empresaBd);
 
-        }
+				if (tbl_resolucion_actual == null)
+				{
+					// crea el registro en base de datos
+					tbl_resolucion = Crear(tbl_resolucion);
 
-        /// <summary>
-        /// Convierte un Objeto de servicio en un Objeto de Base de Datos
-        /// </summary>
-        /// <param name="item">Resolucion obtenida de la DIAN</param>
-        /// <param name="empresa">Identificacion del facturador electrónico</param>
-        /// <returns>Tbl de resolucion</returns>
-        public static TblEmpresasResoluciones Convertir(RangoFacturacion item, TblEmpresas empresa)
-        {
-            TblEmpresasResoluciones tbl_resolucion = new TblEmpresasResoluciones()
-            {
-                StrEmpresa = empresa.StrIdentificacion,
-                StrNumResolucion = item.NumeroResolucion.ToString(),
-                StrPrefijo = (!string.IsNullOrEmpty(item.Prefijo)) ? item.Prefijo : "",
-                IntRangoInicial = Convert.ToInt16(item.RangoInicial),
-                IntRangoFinal = Convert.ToInt16(item.RangoFinal),
-                DatFechaVigenciaDesde = item.FechaVigenciaDesde,
-                DatFechaVigenciaHasta = item.FechaVigenciaHasta,
-                StrClaveTecnica = item.ClaveTecnica,
-                IntIdPlantillaPdf = 0,
-                IntIdPlantillaMail = 0,
-                StrIdSeguridad = Guid.NewGuid(),
-                DatFechaIngreso = Fecha.GetFecha(),
-                DatFechaActualizacion = Fecha.GetFecha()
-            };
+					lista_resolucion.Add(tbl_resolucion);
+				}
+				else
+				{
+					tbl_resolucion_actual.DatFechaVigenciaDesde = tbl_resolucion.DatFechaVigenciaDesde;
+					tbl_resolucion_actual.DatFechaVigenciaHasta = tbl_resolucion.DatFechaVigenciaHasta;
+					tbl_resolucion_actual.IntRangoFinal = tbl_resolucion.IntRangoFinal;
+					tbl_resolucion_actual.IntRangoInicial = tbl_resolucion.IntRangoInicial;
+					tbl_resolucion_actual.StrClaveTecnica = tbl_resolucion.StrClaveTecnica;
+					tbl_resolucion_actual.StrPrefijo = tbl_resolucion.StrPrefijo;
+					tbl_resolucion_actual.DatFechaActualizacion = Fecha.GetFecha();
 
-            return tbl_resolucion;
-        }
+					this.Edit(tbl_resolucion_actual);
+					lista_resolucion.Add(tbl_resolucion_actual);
+				}
+			}
 
-        /// <summary>
-        /// Convierte una Objeto de Base de Datos a un Objeto de respuesta de Resolución
-        /// </summary>
-        /// <param name="resolucion_bd">Resolución obtenida de BD</param>
-        /// <returns>Objeto de respuesta de Resolución</returns>
-        public static Resolucion Convertir(TblEmpresasResoluciones resolucion_bd)
-        {
-            Resolucion resolucion_respuesta = new Resolucion()
-            {
-                ClaveTecnica = resolucion_bd.StrClaveTecnica,
-                FechaResolucion = resolucion_bd.DatFechaVigenciaDesde,
-                FechaVigenciaInicial = resolucion_bd.DatFechaVigenciaDesde,
-                FechaVigenciaFinal = resolucion_bd.DatFechaVigenciaHasta,
-                NumeroResolucion = resolucion_bd.StrNumResolucion,
-                Prefijo = resolucion_bd.StrPrefijo,
-                RangoInicial = resolucion_bd.IntRangoInicial,
-                RangoFinal = resolucion_bd.IntRangoFinal
-            };
+			return lista_resolucion;
 
-            return resolucion_respuesta;
+		}
 
-        }
+		/// <summary>
+		/// Convierte un Objeto de servicio en un Objeto de Base de Datos
+		/// </summary>
+		/// <param name="item">Resolucion obtenida de la DIAN</param>
+		/// <param name="empresa">Identificacion del facturador electrónico</param>
+		/// <returns>Tbl de resolucion</returns>
+		public static TblEmpresasResoluciones Convertir(RangoFacturacion item, TblEmpresas empresa)
+		{
+			TblEmpresasResoluciones tbl_resolucion = new TblEmpresasResoluciones()
+			{
+				StrEmpresa = empresa.StrIdentificacion,
+				StrNumResolucion = item.NumeroResolucion.ToString(),
+				StrPrefijo = (!string.IsNullOrEmpty(item.Prefijo)) ? item.Prefijo : "",
+				IntRangoInicial = Convert.ToInt16(item.RangoInicial),
+				IntRangoFinal = Convert.ToInt16(item.RangoFinal),
+				DatFechaVigenciaDesde = item.FechaVigenciaDesde,
+				DatFechaVigenciaHasta = item.FechaVigenciaHasta,
+				StrClaveTecnica = item.ClaveTecnica,
+				IntIdPlantillaPdf = 0,
+				IntIdPlantillaMail = 0,
+				StrIdSeguridad = Guid.NewGuid(),
+				DatFechaIngreso = Fecha.GetFecha(),
+				DatFechaActualizacion = Fecha.GetFecha()
+			};
 
-    }
+			return tbl_resolucion;
+		}
+
+		/// <summary>
+		/// Convierte una Objeto de Base de Datos a un Objeto de respuesta de Resolución
+		/// </summary>
+		/// <param name="resolucion_bd">Resolución obtenida de BD</param>
+		/// <returns>Objeto de respuesta de Resolución</returns>
+		public static Resolucion Convertir(TblEmpresasResoluciones resolucion_bd)
+		{
+			Resolucion resolucion_respuesta = new Resolucion()
+			{
+				ClaveTecnica = resolucion_bd.StrClaveTecnica,
+				FechaResolucion = resolucion_bd.DatFechaVigenciaDesde,
+				FechaVigenciaInicial = resolucion_bd.DatFechaVigenciaDesde,
+				FechaVigenciaFinal = resolucion_bd.DatFechaVigenciaHasta,
+				NumeroResolucion = resolucion_bd.StrNumResolucion,
+				Prefijo = resolucion_bd.StrPrefijo,
+				RangoInicial = resolucion_bd.IntRangoInicial,
+				RangoFinal = resolucion_bd.IntRangoFinal
+			};
+
+			return resolucion_respuesta;
+
+		}
+
+	}
 }
