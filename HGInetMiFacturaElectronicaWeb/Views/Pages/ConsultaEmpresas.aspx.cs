9dbe31758884a0a149fe9bbfcdf7ba5150d3701e
Atamayo <atamayo.hgi@gmail.com>
2018-05-21 14:02:44 +0000

    permisos gestión y consulta de empresas y usuarios,
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@318 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectronicaWeb/Views/Pages/ConsultaEmpresas.aspx.cs
+++ HGInetMiFacturaElectronicaWeb/Views/Pages/ConsultaEmpresas.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HGInetMiFacturaElectronicaWeb.Properties;
+using HGInetMiFacturaElectronicaWeb.Seguridad;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,8 +9,13 @@ using System.Web.UI.WebControls;
 
 namespace HGInetMiFacturaElectronicaWeb.Views.Pages
 {
-    public partial class ConsultaEmpresas1 : System.Web.UI.Page
+    public partial class ConsultaEmpresas1 : PaginaContenido
     {
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            this.CodigoOpcion = OpcionesPermisos.ConsultaEmpresasSistema;
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
--- HGInetMiFacturaElectronicaWeb/Views/Pages/GestionUsuarios.aspx.cs
+++ HGInetMiFacturaElectronicaWeb/Views/Pages/GestionUsuarios.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HGInetMiFacturaElectronicaWeb.Properties;
+using HGInetMiFacturaElectronicaWeb.Seguridad;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,8 +9,12 @@ using System.Web.UI.WebControls;
 
 namespace HGInetMiFacturaElectronicaWeb.Views.Pages
 {
-    public partial class GestionUsuarios : System.Web.UI.Page
+    public partial class GestionUsuarios : PaginaContenido
     {
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            this.CodigoOpcion = OpcionesPermisos.GestionUsuariosSistema;
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
 
