5d05289e99a27bbb24ca0a65aef468a9c8f31ead
Atamayo <atamayo.hgi@gmail.com>
2018-05-21 14:14:19 +0000

    se añade  base.Page_Load(sender, e) en el load de la página
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@321 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectronicaWeb/Views/Pages/GestionUsuarios.aspx.cs
+++ HGInetMiFacturaElectronicaWeb/Views/Pages/GestionUsuarios.aspx.cs
@@ -17,7 +17,7 @@ namespace HGInetMiFacturaElectronicaWeb.Views.Pages
         }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            base.Page_Load(sender, e);
         }
     }
 }
