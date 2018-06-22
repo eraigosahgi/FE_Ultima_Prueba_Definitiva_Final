d8df45f4219bca3a2d297ac2bae2427a9db1889f
Jflores <jflores.hgi@gmail.com>
2018-06-22 16:45:10 +0000

    Se crea diferencia entre los items publicos y privados
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@558 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectonicaData/Enumerables/ProcesoEstado.cs
+++ HGInetMiFacturaElectonicaData/Enumerables/ProcesoEstado.cs
@@ -10,39 +10,51 @@ namespace HGInetMiFacturaElectonicaData
     public enum ProcesoEstado
     {
         [Description("Recepción")]
+        [AmbientValue("privado")]
         Recepcion = 1,
 
         [Description("Validación Documento")]
+        [AmbientValue("privado")]
         Validacion = 2,
 
         [Description("Generación UBL")]
+        [AmbientValue("privado")]
         UBL = 3,
 
         [Description("Almacenamiento XML")]
+        [AmbientValue("privado")]
         AlmacenaXML = 4,
 
         [Description("Firma XML")]
+        [AmbientValue("privado")]
         FirmaXml = 5,
 
         [Description("Compresión XML")]
+        [AmbientValue("privado")]
         CompresionXml = 6,
 
         [Description("Envío ZIP")]
+        [AmbientValue("publico")]
         EnvioZip = 7,
 
         [Description("Envío E-mail Adquiriente")]
+        [AmbientValue("publico")]
         EnvioEmailAcuse = 8,
 
         [Description("Recepción Acuse")]
+        [AmbientValue("publico")]
         RecepcionAcuse = 9,
 
         [Description("Envío E-mail Acuse")]
+        [AmbientValue("publico")]
         EnvioRespuestaAcuse = 10,
 
 		[Description("Error Dian, Finaliza Proceso")]
-		FinalizacionErrorDian = 90,
+        [AmbientValue("publico")]
+        FinalizacionErrorDian = 90,
 
 		[Description("Fin Proceso Exitoso")]
+        [AmbientValue("publico")]
         Finalizacion = 99
     }
 }
