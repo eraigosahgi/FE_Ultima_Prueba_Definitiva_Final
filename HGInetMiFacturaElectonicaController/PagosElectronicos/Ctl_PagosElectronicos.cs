8d634c020a68611cf3a4142050287e5c7bbeaad5
Atamayo <atamayo.hgi@gmail.com>
2018-07-19 19:32:13 +0000

    se construye objeto con datos de cliente y del pago.
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@689 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectonicaController/PagosElectronicos/Ctl_PagosElectronicos.cs
+++ HGInetMiFacturaElectonicaController/PagosElectronicos/Ctl_PagosElectronicos.cs
@@ -1,4 +1,6 @@
-﻿using HGInetZonaPagos.Clases;
+﻿using HGInetMiFacturaElectonicaController.Registros;
+using HGInetMiFacturaElectonicaData.Modelo;
+using HGInetZonaPagos.Clases;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,26 +24,43 @@ namespace HGInetMiFacturaElectonicaController.PagosElectronicos
             //Ruta retornada por el servicio, redirecciona a la  página de inicio del pago (Selección de tipo persona, forma pago y banco).
             string ruta_inicio = string.Empty;
 
-            //Objetos para reportar el pago
-            HGInetZonaPagos.Cliente datos_cliente = new HGInetZonaPagos.Cliente();
-            HGInetZonaPagos.Pago datos_pago = new HGInetZonaPagos.Pago();
-
-            //LOS CAMPOS EMAIL Y TELEFONO DEL CLIENTE DEBEN SER OBLIGATORIOS.
-
-            // EL PARAMETRO ID_PAGO DEL OBJETO PAGO (datos_pago), CORRESPONDE AL ID DE SEGURIDAD DEL DOCUMENTO.
-            //info_opcional1, info_opcional2 Y info_opcional3 EN DEMO NO PUEDEN SER NULOS.
-
-            //LAS LISTAS lista_codigos_servicio_multicredito, lista_nit_codigos_servicio_multicredito, lista_valores_con_iva Y lista_valores_iva, EN OBJETO PAGO (datos_pago) PUEDEN SER NULL
-
-
             //Datos de la pasarela electrónica.
             int comercio_id = 2651;
             string comercio_clave = "2651HGI";
             string comercio_ruta = "t_pruebasHGI";
+            string codigo_servicio = "2701";
+
+            Ctl_Documento clase_documento = new Ctl_Documento();
+            TblDocumentos datos_documento = clase_documento.ObtenerPorIdSeguridad(id_seguridad_documento).FirstOrDefault();
+
+            //Objetos para reportar el pago
+            HGInetZonaPagos.Cliente datos_cliente = new HGInetZonaPagos.Cliente();
+            datos_cliente.email = datos_documento.TblEmpresasAdquiriente.StrMail;
+            datos_cliente.id_cliente = datos_documento.StrEmpresaAdquiriente;
+            datos_cliente.nombre = datos_documento.TblEmpresasAdquiriente.StrRazonSocial;
+            //Empresa no tiene telefono.
+            datos_cliente.telefono = "444 45 84";
+            datos_cliente.tipo_id = datos_documento.IntDocTipo.ToString();
+
+            HGInetZonaPagos.Pago datos_pago = new HGInetZonaPagos.Pago();
+
+            datos_pago.id_pago = id_seguridad_documento.ToString();
+            datos_pago.descripcion_pago = string.Format("{0}", datos_pago.id_pago);
+            datos_pago.total_con_iva = Convert.ToDouble(datos_documento.IntVlrTotal);
+            datos_pago.valor_iva = 0;
+            datos_pago.codigo_servicio_principal = codigo_servicio;
+            datos_pago.info_opcional1 = "";
+            datos_pago.info_opcional2 = "";
+            datos_pago.info_opcional3 = "";
+            datos_pago.lista_codigos_servicio_multicredito = null;
+            datos_pago.lista_nit_codigos_servicio_multicredito = null;
+            datos_pago.lista_valores_con_iva = null;
+            datos_pago.lista_valores_iva = null;
+            datos_pago.total_codigos_servicio = 0;
 
             ProcesoPago clase_proceso_pago = new ProcesoPago(comercio_id, comercio_clave, comercio_ruta);
 
-            ruta_inicio = clase_proceso_pago.ReportarPago(datos_cliente, datos_pago);
+            ruta_inicio = clase_proceso_pago.ReportarPago(datos_cliente, datos_pago, true);
 
             return ruta_inicio;
         }
