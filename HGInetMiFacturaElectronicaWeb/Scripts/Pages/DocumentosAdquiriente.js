ad706d9f2e95d37c97e6487bd36335ff64985f7f
Jflores <jflores.hgi@gmail.com>
2018-09-10 14:16:59 +0000

    -Se modifica la direccion de las rutas de la plataforma intermedia
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@903 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectronicaWeb/Scripts/Pages/DocumentosAdquiriente.js
+++ HGInetMiFacturaElectronicaWeb/Scripts/Pages/DocumentosAdquiriente.js
@@ -635,7 +635,7 @@ DocAdquirienteApp.controller('ModalPagosController', function ModalPagosControll
         $scope.EnProceso = true;
         $http.get('/api/Documentos?strIdSeguridad=' + $scope.IdSeguridad + '&tipo_pago = 0 &registrar_pago=true&valor_pago=' + $scope.valoraPagar).then(function (response) {
 
-            var RutaServicio = "http://localhost:50145/Views/Pago.aspx?IdSeguridad=";
+            var RutaServicio = "http://cloudservices.hginet.co/Views/Pago.aspx?IdSeguridad=";
 
             $scope.Idregistro = response.data.IdRegistro;
 
@@ -673,9 +673,9 @@ DocAdquirienteApp.controller('ModalPagosController', function ModalPagosControll
 
     function VerificarEstado() {
         $('#wait').show();
-
-
-        $http.get('http://localhost:50145/api/VerificarEstado?IdSeguridadPago=' + $scope.IdSeguridad + "&StrIdSeguridadRegistro=" + $scope.Idregistro).then(function (response) {
+        
+        //$http.get('http://localhost:50145/api/VerificarEstado?IdSeguridadPago=' + $scope.IdSeguridad + "&StrIdSeguridadRegistro=" + $scope.Idregistro).then(function (response) {
+        $http.get('http://cloudservices.hginet.co/api/VerificarEstado?IdSeguridadPago=' + $scope.IdSeguridad + "&StrIdSeguridadRegistro=" + $scope.Idregistro).then(function (response) {
             //Esto retorna un objeto  de la plataforma intermedia que sirve para actualizar el pago local
             var ObjeRespuestaPI = response.data;
             //////////////////////////////////////////////////////////////////////
