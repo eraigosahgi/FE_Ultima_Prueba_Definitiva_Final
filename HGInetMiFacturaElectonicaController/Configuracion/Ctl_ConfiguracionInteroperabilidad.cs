83c705d4aee0ba025bd98a9b6fd104b6cbd385bd
Mparamo <mparamo@noemail.com>
2018-09-27 18:42:55 +0000

    Se crea el controlador de configuración de interoperabilidad (acceso a datos)
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@976 30a10932-8c21-4e4d-906a-15261f94da71

---
+++ HGInetMiFacturaElectonicaController/Configuracion/Ctl_ConfiguracionInteroperabilidad.cs
@@ -0,0 +1,70 @@
+﻿using HGInetMiFacturaElectonicaData;
+using HGInetMiFacturaElectonicaData.ControllerSql;
+using HGInetMiFacturaElectonicaData.Modelo;
+using LibreriaGlobalHGInet.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HGInetMiFacturaElectonicaController.Configuracion
+{
+	public class Ctl_ConfiguracionInteroperabilidad : BaseObject<TblConfiguracionInteroperabilidad>
+	{
+		#region Constructores 
+		public Ctl_ConfiguracionInteroperabilidad() : base(new ModeloAutenticacion()) { }
+		public Ctl_ConfiguracionInteroperabilidad(ModeloAutenticacion autenticacion) : base(autenticacion) { }
+		public Ctl_ConfiguracionInteroperabilidad(string servidor, string basedatos, string usuario, string clave) : base(servidor, basedatos, usuario, clave) { }
+		#endregion
+
+		/// <summary>
+		/// Obtiene la configuración con la identificacion del proveedor tecnológico
+		/// </summary>
+		/// <param name="identificacion">Identificacion del proveedor tecnológico</param>
+		/// <returns>configuración</returns>
+		public TblConfiguracionInteroperabilidad Obtener(string identificacion)
+		{
+
+			var datos = (from item in context.TblConfiguracionInteroperabilidad
+						 where item.StrIdentificacion.Equals(identificacion)
+						 select item).FirstOrDefault();
+
+			return datos;
+		}
+
+		/// <summary>
+		/// Obtiene la configuración con el id de seguridad del proveedor tecnológico
+		/// </summary>
+		/// <param name="identificacion">Identificacion del proveedor tecnológico</param>
+		/// <returns>configuración</returns>
+		public TblConfiguracionInteroperabilidad Obtener(Guid id_seguridad)
+		{
+			var datos = (from item in context.TblConfiguracionInteroperabilidad
+						 where item.StrIdSeguridad.Equals(id_seguridad)
+						 select item).FirstOrDefault();
+
+			return datos;
+		}
+
+		/// <summary>
+		/// Valida Autenticacion del Proveedor Tecnológico
+		/// </summary>
+		/// <param name="usuario">usuario</param>
+		/// <param name="clave">clave sin encripción</param>
+		/// <returns>datos de configuración si es encontrado o null si no</returns>
+		public TblConfiguracionInteroperabilidad Validar(string usuario, string clave)
+		{
+			// se encripta la clave en SHA256 para comparación con la base de datos
+			string clave_sha256 = Encriptar.Encriptar_SHA256(clave);
+
+			TblConfiguracionInteroperabilidad datos = (from item in context.TblConfiguracionInteroperabilidad
+													   where item.StrUsuario.Equals(usuario) && item.StrClave.Equals(clave_sha256)
+													   select item).FirstOrDefault();
+
+			return datos;
+		}
+
+
+	}
+}
