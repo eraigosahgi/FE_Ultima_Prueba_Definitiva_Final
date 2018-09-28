3d1b6f333c7d05174aed014af289c2ac0c96a730
Atamayo <atamayo.hgi@gmail.com>
2018-09-28 16:32:37 +0000

    se crea el campo IntIdEstadoPago en TblDocumentos
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@984 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectonicaData/Modelo/ModeloDatos.Context.cs
+++ HGInetMiFacturaElectonicaData/Modelo/ModeloDatos.Context.cs
@@ -38,7 +38,7 @@ namespace HGInetMiFacturaElectonicaData.Modelo
         public virtual DbSet<TblPagosElectronicos> TblPagosElectronicos { get; set; }
         public virtual DbSet<TblEmpresasResoluciones> TblEmpresasResoluciones { get; set; }
         public virtual DbSet<TblFormatos> TblFormatos { get; set; }
-        public virtual DbSet<TblDocumentos> TblDocumentos { get; set; }
         public virtual DbSet<TblConfiguracionInteroperabilidad> TblConfiguracionInteroperabilidad { get; set; }
+        public virtual DbSet<TblDocumentos> TblDocumentos { get; set; }
     }
 }
