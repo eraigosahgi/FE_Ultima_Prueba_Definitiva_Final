32fa29b51612dac209325809fef306fa93df2fed
Jzea <jzea.hgi@gmail.com>
2018-09-28 21:12:44 +0000

    -Se borra metodo de prueba para comprimir una lista
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@990 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectonicaController/Procesos/Ctl_Compresion.cs
+++ HGInetMiFacturaElectonicaController/Procesos/Ctl_Compresion.cs
@@ -64,51 +64,5 @@ namespace HGInetMiFacturaElectonicaController.Procesos
             return documento.NombreZip;
         }
 
-
-        /// <summary>
-        /// Proceso para generar el archivo Zip que se debe enviar al proveedor receptor
-        /// </summary>
-        /// <param name="ruta_zip"></param>
-        /// <param name="ruta_archivos"></param>
-        /// <param name="NombreArchivo"></param>
-        /// <returns></returns>
-        public static string ComprimirLista(string ruta_zip, string ruta_archivos, string NombreArchivo)
-        {
-
-
-
-            try
-            {
-                // genera la compresión del archivo en zip
-                ZipArchive archive = ZipFile.Open(ruta_zip, ZipArchiveMode.Update);
-                //Aqui se debe iterear una lista que debe enviar el proceso anterior para poder ubicar los archivos que se van a comprimir
-                for (int i = 1; i < 5; i++)
-                {
-
-
-                    string Arc = @"E:\Desarrollo\jflores\Proyectos\HGINetMiFacturaElectronica\Codigo\HGInetMiFacturaElectronicaWeb\dms\eb821fbe-02ba-4cfc-a7a9-248711513591\FacturaEDian\face_f0811021438003B0235A" + i + ".pdf";
-
-                    // elimina el archivo zip si existe
-                    //  if (!Archivo.ValidarExistencia(Arc))
-                    //{
-                    string ss = Path.GetFileName(Arc);
-
-                    archive.CreateEntryFromFile(Arc, Path.GetFileName(Arc));
-                    //}
-
-                }
-
-                archive.Dispose();
-
-
-                return "";
-            }
-            catch (Exception excepcion)
-            {
-                return excepcion.ToString();
-
-            }
-        }
-
     }
 }
