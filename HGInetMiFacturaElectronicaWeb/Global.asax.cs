b72c8b81540ea751bf5c0af5ecc3dc3871946d3b
Jzea <jzea.hgi@gmail.com>
2018-10-19 18:03:59 +0000

    -Se agrega proceso para guardar la peticion recibida -Se crea clase y metodos para el guardado de la peticion como log
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@1078 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectronicaWeb/Global.asax.cs
+++ HGInetMiFacturaElectronicaWeb/Global.asax.cs
@@ -9,11 +9,29 @@ using System.Web.Security;
 using System.Web.SessionState;
 using System.Web.Http;
 using Newtonsoft.Json;
+using HGInetMiFacturaElectronicaWeb.Controllers.Services;
+using System.Configuration;
+using System.IO;
+using System.Text;
+using LibreriaGlobalHGInet.General;
 
 namespace HGInetMiFacturaElectronicaWeb
 {
 	public class Global : HttpApplication
 	{
+
+		protected static string fileNameBase;
+		protected static string ext = "log";
+
+		// One file name per day
+		protected string FileName
+		{
+			get
+			{
+				return String.Format("{0}{1}.{2}", fileNameBase, DateTime.Now.ToString("yyyy-MM-dd"), ext);
+			}
+		}
+
 		void Application_Start(object sender, EventArgs e)
 		{
 			// Código que se ejecuta al iniciar la aplicación
@@ -62,6 +80,33 @@ namespace HGInetMiFacturaElectronicaWeb
 				response.End();
 			}
 			RegisterRoutes(RouteTable.Routes);
+
+			//Proceso para guardar la peticion cuando sea un servicio
+			string servicio = context.Request.FilePath.ToLowerInvariant();
+
+			string id = string.Empty;
+
+			try
+			{
+				if (servicio.Contains("wcf"))
+				{
+					//Proceso tomado de https://blogs.msdn.microsoft.com/rodneyviana/2014/02/06/logging-incoming-requests-and-responses-in-an-asp-net-or-wcf-application-in-compatibility-mode/
+					// Crea un guid por cada peticion
+					id = Guid.NewGuid().ToString();
+					//guarda la peticion
+					PeticionHttp input = new PeticionHttp(context, Request.Filter, id, servicio);
+					Request.Filter = input;
+					input.SetFilter(false);
+					//guarda la respuesta
+					PeticionHttp output = new PeticionHttp(context, Response.Filter, id, servicio);
+					output.SetFilter(true);
+					Response.Filter = output;
+				}
+			}
+			catch (Exception excepcion)
+			{
+				LogExcepcion.Guardar(new Exception(string.Format("Error al procesar la peticion {0}", excepcion), excepcion));
+			}
 		}
 
 		void RegisterRoutes(RouteCollection routes)
@@ -74,5 +119,6 @@ namespace HGInetMiFacturaElectronicaWeb
 		{
 			// Code that runs when an unhandled error occurs
 		}
+
 	}
 }
