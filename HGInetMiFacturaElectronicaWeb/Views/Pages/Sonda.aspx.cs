f37a85676b6e9921887b17382585777d76ef70af
Jflores <jflores.hgi@gmail.com>
2018-10-24 19:38:16 +0000

    -Se cambia la logica de procesar documentos que estaba en javascript, para el load de la pagina cn codigo c#
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@1096 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectronicaWeb/Views/Pages/Sonda.aspx.cs
+++ HGInetMiFacturaElectronicaWeb/Views/Pages/Sonda.aspx.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HGInetMiFacturaElectonicaController.Registros;
+using HGInetMiFacturaElectonicaData.Modelo;
+using HGInetMiFacturaElectonicaData.ModeloServicio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +14,21 @@ namespace HGInetMiFacturaElectronicaWeb.Views.Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                Ctl_Documento ctl_documento = new Ctl_Documento();
+                List<TblDocumentos> datos = ctl_documento.ObtenerDocumentosaProcesar();
+
+                lblResultado.Text = string.Format("Numero de Documentos a procesar: {0} ", datos.Count().ToString());
+
+                List<DocumentoRespuesta> datosProcesar = HGInetMiFacturaElectonicaController.Procesos.Ctl_Documentos.Procesar(datos);
+            }
+            catch (Exception ex)
+            {
+
+                lblResultado.Text = string.Format("El proceso genero un error: {0}",ex.Message);
+            }
+
 
         }
     }
