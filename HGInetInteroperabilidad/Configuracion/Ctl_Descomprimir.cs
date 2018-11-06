8d30a024aa186b0ffce3815449983917bcd50750
Jzea <jzea.hgi@gmail.com>
2018-11-06 22:20:27 +0000

    -Se cambia validacion con la cantidad de archivos enviados.
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@1145 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetInteroperabilidad/Configuracion/Ctl_Descomprimir.cs
+++ HGInetInteroperabilidad/Configuracion/Ctl_Descomprimir.cs
@@ -18,117 +18,117 @@ using System.Threading.Tasks;
 
 namespace HGInetInteroperabilidad.Configuracion
 {
-    public class Ctl_Descomprimir
-    {
+	public class Ctl_Descomprimir
+	{
 
-        public static RegistroListaDocRespuesta Procesar(RegistroListaDoc datos, string proveedor_emisor)
-        {
-            
-
-            RegistroListaDocRespuesta datos_respuesta = new RegistroListaDocRespuesta();
-
-            if (datos == null)
-            {
-                datos_respuesta.timeStamp = Fecha.FechaUtc(DateTime.Now);
-                datos_respuesta.trackingIds = null;
-                datos_respuesta.mensajeGlobal = string.Format("{2}|No se obtuvo informacion de {0} {1}", datos.nombre, Enumeracion.GetDescription(RespuestaInterOperabilidad.Zipvacio), RespuestaInterOperabilidad.Zipvacio.GetHashCode());
-
-            }
-            PlataformaData plataforma_datos = HgiConfiguracion.GetConfiguration().PlataformaData;
-
-            //Obtengo el proveedor emisor
-            TblConfiguracionInteroperabilidad proveedor = new TblConfiguracionInteroperabilidad();
-
-            Ctl_ConfiguracionInteroperabilidad configuracion = new Ctl_ConfiguracionInteroperabilidad();
-
-            proveedor = configuracion.Obtener(proveedor_emisor);
-
-            string destino = string.Empty;
-
-            //Obtengo archivos para procesar
-            if (!Archivo.ValidarExistencia(string.Format(@"{0}{1}{2}\{3}", plataforma_datos.RutaDmsFisica,Constantes.RutaInteroperabilidadFtp,proveedor.StrIdSeguridad, datos.nombre)))
-            {
-                datos_respuesta.timeStamp = Fecha.FechaUtc(DateTime.Now);
-                datos_respuesta.trackingIds = null;
-                datos_respuesta.mensajeGlobal = string.Format("{2}|No se obtuvo informacion de {0} {1}", datos.nombre, Enumeracion.GetDescription(RespuestaInterOperabilidad.Zipvacio), RespuestaInterOperabilidad.Zipvacio.GetHashCode());
-
-            }
-            else
-            {
-
-                string ruta_fisica_zip = string.Format("{0}{1}{2}\\{3}", plataforma_datos.RutaDmsFisica, Constantes.RutaInteroperabilidadFtp, proveedor.StrIdSeguridad, datos.nombre);
-                
-                string ruta_archivos = string.Format("{0}\\{1}\\{2}", plataforma_datos.RutaDmsFisica, Constantes.RutaInteroperabilidadRecepcion, proveedor.StrIdSeguridad);
-
-                Directorio.CrearDirectorio(ruta_archivos);
+		public static RegistroListaDocRespuesta Procesar(RegistroListaDoc datos, string proveedor_emisor)
+		{
 
 
-                try
-                {
-                    // Ingreso a la Carpeta para validar informacion
-                    using (ZipArchive file = ZipFile.OpenRead(ruta_fisica_zip))
-                    {
-                        if (file.Entries.Count > 100)
-                        {
-                            datos_respuesta.timeStamp = Fecha.FechaUtc(DateTime.Now);
-                            datos_respuesta.trackingIds = null;
-                            datos_respuesta.mensajeGlobal = string.Format("No se obtuvo informacion de {0} {1}", datos.nombre, Enumeracion.GetDescription(RespuestaInterOperabilidad.ZipSuperaMaximo));
+			RegistroListaDocRespuesta datos_respuesta = new RegistroListaDocRespuesta();
 
-                        }
-                        else
-                        {
-                            //Recorro la carpeta
-                            foreach (ZipArchiveEntry archivo in file.Entries)
-                            {
-                                //Valida que el archivo no tenga ninguna de estas extensiones
-                                if ((archivo.FullName.EndsWith(".exe", StringComparison.OrdinalIgnoreCase)) || (archivo.FullName.EndsWith(".bat", StringComparison.OrdinalIgnoreCase))
-                                    || (archivo.FullName.EndsWith(".accdb", StringComparison.OrdinalIgnoreCase)) || (archivo.FullName.EndsWith(".mdb", StringComparison.OrdinalIgnoreCase)))
-                                {
-                                    archivo.Delete();
-                                }
-                            }
-                            // Obtiene la ruta completa para garantizar que se eliminen los segmentos relativos.
-                            destino = Path.GetFullPath(Path.Combine(ruta_archivos, Path.GetFileNameWithoutExtension(datos.nombre)));
+			if (datos == null)
+			{
+				datos_respuesta.timeStamp = Fecha.FechaUtc(DateTime.Now);
+				datos_respuesta.trackingIds = null;
+				datos_respuesta.mensajeGlobal = string.Format("{2}|No se obtuvo informacion de {0} {1}", datos.nombre, Enumeracion.GetDescription(RespuestaInterOperabilidad.Zipvacio), RespuestaInterOperabilidad.Zipvacio.GetHashCode());
 
-                            // genera la descompresión del archivo zip
-                            file.ExtractToDirectory(destino);
-                        }
-                        file.Dispose();
-                    }
+			}
+			PlataformaData plataforma_datos = HgiConfiguracion.GetConfiguration().PlataformaData;
 
-                    
-                    //Archivo.Borrar(@"C:\inetpub\vhosts\mifacturaenlinea.com.co\fileshab.mifacturaenlinea.com.co\interoperabilidad\publico\f791dbce-c640-4f32-99d4-5966a573701f\811021438_123456_86cfbb40-5dde-4908-bbd1-fc2ae7c6e8b6.zip");
-                    //ruta_fisica_zip = string.Format(@"{0}{1}{2}\{3}", plataforma_datos.RutaDmsFisica, Constantes.RutaInteroperabilidadFtp.Replace("\\", @"\"), proveedor.StrIdSeguridad, datos.nombre);
-                    
+			//Obtengo el proveedor emisor
+			TblConfiguracionInteroperabilidad proveedor = new TblConfiguracionInteroperabilidad();
 
-                    //Envia la informacion para procesarla
-                    datos_respuesta = Ctl_Recepcion.Procesar(datos, destino, proveedor.StrIdentificacion);
-                    
-                }
-                catch (Exception excepcion)
-                {
-                    LogExcepcion.Guardar(excepcion);
-                    datos_respuesta.timeStamp = Fecha.FechaUtc(DateTime.Now);
-                    datos_respuesta.trackingIds = null;
-                    datos_respuesta.mensajeGlobal = string.Format("{2}|Error al descomprimir {0} {1}", datos.nombre, Enumeracion.GetDescription(RespuestaInterOperabilidad.ErrorInternoReceptor), RespuestaInterOperabilidad.ErrorInternoReceptor);
-                    //throw new ApplicationException(string.Format("Error al descomprimir {0} Detalle: {1}", datos.nombre, excepcion.Message));
-                }
-            }
+			Ctl_ConfiguracionInteroperabilidad configuracion = new Ctl_ConfiguracionInteroperabilidad();
 
-            //Aqui elimino el archivo Zip si todo esta OK
-            try
-            {
-                Archivo.Borrar(string.Format(@"{0}{1}{2}\{3}", plataforma_datos.RutaDmsFisica, Constantes.RutaInteroperabilidadFtp, proveedor.StrIdSeguridad, datos.nombre));
-            }
-            catch (Exception excepcion)
-            {
-                LogExcepcion.Guardar(excepcion);
+			proveedor = configuracion.Obtener(proveedor_emisor);
 
-            }
+			string destino = string.Empty;
 
-            return datos_respuesta;
-        }
+			//Obtengo archivos para procesar
+			if (!Archivo.ValidarExistencia(string.Format(@"{0}{1}{2}\{3}", plataforma_datos.RutaDmsFisica, Constantes.RutaInteroperabilidadFtp, proveedor.StrIdSeguridad, datos.nombre)))
+			{
+				datos_respuesta.timeStamp = Fecha.FechaUtc(DateTime.Now);
+				datos_respuesta.trackingIds = null;
+				datos_respuesta.mensajeGlobal = string.Format("{2}|No se obtuvo informacion de {0} {1}", datos.nombre, Enumeracion.GetDescription(RespuestaInterOperabilidad.Zipvacio), RespuestaInterOperabilidad.Zipvacio.GetHashCode());
+
+			}
+			else
+			{
+				if (datos.documentos.Count > 100)
+				{
+					datos_respuesta.timeStamp = Fecha.FechaUtc(DateTime.Now);
+					datos_respuesta.trackingIds = null;
+					datos_respuesta.mensajeGlobal = string.Format("{2}|El zip {0} contiene mas de 100 documentos de {1}", datos.nombre, Enumeracion.GetDescription(RespuestaInterOperabilidad.ZipSuperaMaximo), RespuestaInterOperabilidad.ZipSuperaMaximo.GetHashCode());
+
+				}
+				else
+				{
+					string ruta_fisica_zip = string.Format("{0}{1}{2}\\{3}", plataforma_datos.RutaDmsFisica, Constantes.RutaInteroperabilidadFtp, proveedor.StrIdSeguridad, datos.nombre);
+
+					string ruta_archivos = string.Format("{0}\\{1}\\{2}", plataforma_datos.RutaDmsFisica, Constantes.RutaInteroperabilidadRecepcion, proveedor.StrIdSeguridad);
+
+					Directorio.CrearDirectorio(ruta_archivos);
+
+					try
+					{
+						// Ingreso a la Carpeta para validar informacion
+						using (ZipArchive file = ZipFile.OpenRead(ruta_fisica_zip))
+						{
+
+							//Recorro la carpeta
+							foreach (ZipArchiveEntry archivo in file.Entries)
+							{
+								//Valida que el archivo no tenga ninguna de estas extensiones
+								if ((archivo.FullName.EndsWith(".exe", StringComparison.OrdinalIgnoreCase)) || (archivo.FullName.EndsWith(".bat", StringComparison.OrdinalIgnoreCase))
+									|| (archivo.FullName.EndsWith(".accdb", StringComparison.OrdinalIgnoreCase)) || (archivo.FullName.EndsWith(".mdb", StringComparison.OrdinalIgnoreCase)))
+								{
+									archivo.Delete();
+								}
+							}
+							// Obtiene la ruta completa para garantizar que se eliminen los segmentos relativos.
+							destino = Path.GetFullPath(Path.Combine(ruta_archivos, Path.GetFileNameWithoutExtension(datos.nombre)));
+
+							// genera la descompresión del archivo zip
+							file.ExtractToDirectory(destino);
+
+							file.Dispose();
+						}
 
 
-    }
+						//Archivo.Borrar(@"C:\inetpub\vhosts\mifacturaenlinea.com.co\fileshab.mifacturaenlinea.com.co\interoperabilidad\publico\f791dbce-c640-4f32-99d4-5966a573701f\811021438_123456_86cfbb40-5dde-4908-bbd1-fc2ae7c6e8b6.zip");
+						//ruta_fisica_zip = string.Format(@"{0}{1}{2}\{3}", plataforma_datos.RutaDmsFisica, Constantes.RutaInteroperabilidadFtp.Replace("\\", @"\"), proveedor.StrIdSeguridad, datos.nombre);
+
+
+						//Envia la informacion para procesarla
+						datos_respuesta = Ctl_Recepcion.Procesar(datos, destino, proveedor.StrIdentificacion);
+
+					}
+					catch (Exception excepcion)
+					{
+						LogExcepcion.Guardar(excepcion);
+						datos_respuesta.timeStamp = Fecha.FechaUtc(DateTime.Now);
+						datos_respuesta.trackingIds = null;
+						datos_respuesta.mensajeGlobal = string.Format("{2}|Error al descomprimir {0} {1}", datos.nombre, Enumeracion.GetDescription(RespuestaInterOperabilidad.ErrorInternoReceptor), RespuestaInterOperabilidad.ErrorInternoReceptor);
+						//throw new ApplicationException(string.Format("Error al descomprimir {0} Detalle: {1}", datos.nombre, excepcion.Message));
+					}
+				}
+			}
+
+			//Aqui elimino el archivo Zip si todo esta OK
+			try
+			{
+				Archivo.Borrar(string.Format(@"{0}{1}{2}\{3}", plataforma_datos.RutaDmsFisica, Constantes.RutaInteroperabilidadFtp, proveedor.StrIdSeguridad, datos.nombre));
+			}
+			catch (Exception excepcion)
+			{
+				LogExcepcion.Guardar(excepcion);
+
+			}
+
+			return datos_respuesta;
+		}
+
+
+	}
 }
