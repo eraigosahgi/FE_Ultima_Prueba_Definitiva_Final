43b945ef65ecb9bd361f839eb2ea5454195ded56
Jzea <jzea.hgi@gmail.com>
2018-12-03 15:34:26 +0000

    -Se agrega pagina para la Sondade resoluciones con la logica para ejecutarla
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@1212 30a10932-8c21-4e4d-906a-15261f94da71

---
+++ HGInetMiFacturaElectronicaWeb/Views/Pages/SondaResoluciones.aspx.cs
@@ -0,0 +1,34 @@
+﻿using HGInetMiFacturaElectonicaController.Procesos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HGInetMiFacturaElectronicaWeb.Views.Pages
+{
+	public partial class SondaResoluciones : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			try
+			{
+				Procesar();
+			}
+			catch (Exception ex)
+			{
+				lblResultado.Text = string.Format("El proceso genero un error {0}", ex.Message);
+			}
+		}
+
+
+		public void Procesar()
+		{
+			Ctl_Resoluciones ctl_resoluciones = new Ctl_Resoluciones();
+
+			var Tarea1 = ctl_resoluciones.SondaObtenerResoluciones();
+			lblResultado.Text = string.Format("Termino");
+		}
+	}
+}
