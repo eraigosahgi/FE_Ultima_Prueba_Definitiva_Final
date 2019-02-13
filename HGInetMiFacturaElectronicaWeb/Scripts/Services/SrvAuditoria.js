88ae76ea3dca4c9a628524325d6050eac554cb67
Jzea <jzea.hgi@gmail.com>
2019-02-13 14:08:49 +0000

    -Se agrega js de Auditoria
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@1381 30a10932-8c21-4e4d-906a-15261f94da71

---
+++ HGInetMiFacturaElectronicaWeb/Scripts/Services/SrvAuditoria.js
@@ -0,0 +1,17 @@
+﻿DevExpress.localization.locale(navigator.language);
+
+var AppSrvAuditoria = angular.module('AppSrvAuditoria', ['dx'])
+ .config(function ($httpProvider) {
+ 	$httpProvider.interceptors.push(myInterceptor);
+ })
+ .service('SrvAuditoria', function ($http, $location, $q) {
+ 	//Obtiene los documentos Admin
+ 	this.ConsultaAuditoria = function (fecha_inicio, fecha_fin, cod_facturador, numero_documento, estado_dian, proceso_doc, tipo_registro, procedencia) {
+ 		return $http.get('/Api/ConsultaAuditoria?fecha_inicio=' + fecha_inicio + '&fecha_fin=' + fecha_fin + '&identificacion_obligado=' + cod_facturador + '&estado=' + estado_dian + '&proceso=' + proceso_doc + '&tipo_registro=' + tipo_registro + '&procedencia=' + procedencia + '&numero_documento=' + numero_documento).then(function (response) {
+ 			return response.data;
+ 		}, function (response) {
+ 			DevExpress.ui.notify(response.data.ExceptionMessage, 'error', 3000);
+ 			return $q.reject(response.data);
+ 		});
+ 	}
+ });
