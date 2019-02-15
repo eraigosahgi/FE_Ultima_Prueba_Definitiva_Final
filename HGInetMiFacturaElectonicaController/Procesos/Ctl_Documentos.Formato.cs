6a976f222e79f3f87ae9ecc0ce607c52e383f417
Atamayo <atamayo.hgi@gmail.com>
2019-02-15 21:13:49 +0000

    se incluye funcionalidad de formatos para la captura del diseño desde base de datos o telerik.
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@1403 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectonicaController/Procesos/Ctl_Documentos.Formato.cs
+++ HGInetMiFacturaElectonicaController/Procesos/Ctl_Documentos.Formato.cs
@@ -43,42 +43,44 @@ namespace HGInetMiFacturaElectonicaController.Procesos
 			{
 				var documento_obj = (dynamic)null;
 				documento_obj = documento;
-				respuesta.UrlPdf = string.Empty;
+
+				bool generar_pdf = true;
 
 				PlataformaData plataforma_datos = HgiConfiguracion.GetConfiguration().PlataformaData;
 
 
 				// valida formato en base 64 del objeto
 				Formato formato_documento = (Formato)(dynamic)documento_obj.DocumentoFormato;
-				//valido si el formato es enviado del ERP
-				if (formato_documento.Codigo == -1 || formato_documento.Codigo == 0)
+				if (formato_documento != null)
 				{
 					if (!string.IsNullOrEmpty(formato_documento.ArchivoPdf))
 					{
 						// almacena archivo en base64
 						documento_result.NombrePdf = Ctl_Formato.GuardarArchivo(formato_documento.ArchivoPdf, documento_result);
+						respuesta.UrlPdf = string.Empty;
 
 						if (!string.IsNullOrWhiteSpace(documento_result.NombrePdf))
 						{
+
 							// url pública del pdf
 							string url_ppal_pdf = string.Format("{0}/{1}/{2}", plataforma_datos.RutaDmsPublica, Constantes.CarpetaFacturaElectronica, documento_result.IdSeguridadTercero.ToString());
+
 							respuesta.UrlPdf = string.Format(@"{0}/{1}/{2}.pdf", url_ppal_pdf, LibreriaGlobalHGInet.Properties.RecursoDms.CarpetaFacturaEDian, documento_result.NombrePdf);
 
+							documentoBd.StrUrlArchivoPdf = respuesta.UrlPdf;
 						}
 
-					}
-					else
-					{
-						throw new ArgumentException(string.Format("No se encontró Formato PDF del documento {0} ", documento_obj.Documento));
+						generar_pdf = false;
 					}
 				}
-				else
+
+				if (generar_pdf)
 				{
 
 					string url_ppal_pdf = string.Format("{0}/{1}/{2}", plataforma_datos.RutaDmsPublica, Constantes.CarpetaFacturaElectronica, documento_result.IdSeguridadTercero.ToString());
 					string ruta = string.Format(@"{0}/{1}/", url_ppal_pdf, LibreriaGlobalHGInet.Properties.RecursoDms.CarpetaFacturaEDian);
 					Report reporte_pdf = new Report();
-
+					bool reporte_archivado = true;
 					/*
                      Para la contrucción de formatos se debe tener en cuenta el envío del parametro TipoDocumento de caracter obligatorio
                      para el reporte pdf, ya que este valor es implementado para la carga de información desde cada formato.
@@ -89,22 +91,24 @@ namespace HGInetMiFacturaElectonicaController.Procesos
 					Ctl_Formatos clase_formatos = new Ctl_Formatos();
 					TblFormatos datos_formato = clase_formatos.ObtenerFormato(formato_documento.Codigo, documentoBd.StrEmpresaFacturador, TipoFormato.FormatoPDF.GetHashCode());
 
+					List<DocumentoDetalle> detalles_formato = new List<DocumentoDetalle>();
+					//Recorre los detalles y agrega los items visibles.
+					foreach (var item in documento_obj.DocumentoDetalles)
+					{
+						if (item.OcultarItem == 0)
+							detalles_formato.Add(item);
+					}
+
+					documento_obj.DocumentoDetalles = detalles_formato;
+
+
 					if (datos_formato != null)
 					{
+						reporte_archivado = false;
 						XtraReportDesigner rep = new XtraReportDesigner();
 
 						MemoryStream datos = new MemoryStream(datos_formato.Formato);
 						rep.LoadLayoutFromXml(datos);
-
-						List<DocumentoDetalle> detalles_formato = new List<DocumentoDetalle>();
-						//Recorre los detalles y agrega los items visibles.
-						foreach (var item in documento_obj.DocumentoDetalles)
-						{
-							if (item.OcultarItem == 0)
-								detalles_formato.Add(item);
-						}
-
-						documento_obj.DocumentoDetalles = detalles_formato;
 
 						rep.DataSource = documento_obj;
 						HGInetFacturaEReports.Reporte x = new HGInetFacturaEReports.Reporte(documento_result.NombreXml, documento_result.RutaArchivosEnvio);
@@ -112,21 +116,75 @@ namespace HGInetMiFacturaElectonicaController.Procesos
 					}
 					else
 					{
-						throw new ApplicationException(string.Format("El formato N.{0} no se encuentra disponible para el facturador {1}.", formato_documento.Codigo, documentoBd.StrEmpresaFacturador));
+						switch (formato_documento.Codigo)
+						{
+							case 1:
+								switch (documento_result.DocumentoTipo)
+								{
+									case TipoDocumento.Factura:
+										reporte_pdf = new HGInetFacturaEReports.Facturas.Formato1();
+										break;
+									case TipoDocumento.NotaDebito:
+										reporte_pdf = new HGInetFacturaEReports.NotasDebito.Formato1();
+										break;
+									case TipoDocumento.NotaCredito:
+										reporte_pdf = new HGInetFacturaEReports.NotasCredito.Formato1();
+										break;
+								}
+								break;
+							case 2:
+								reporte_pdf = new HGInetFacturaEReports.Facturas.Formato2();
+								reporte_pdf.ReportParameters["TipoDocumento"].Value = documento_result.DocumentoTipo.GetHashCode();
+								break;
+							case 3:
+								reporte_pdf = new HGInetFacturaEReports.Facturas.Formato3();
+								reporte_pdf.ReportParameters["TipoDocumento"].Value = documento_result.DocumentoTipo.GetHashCode();
+								break;
+							case 4:
+								reporte_pdf = new HGInetFacturaEReports.Facturas.Formato4();
+								reporte_pdf.ReportParameters["TipoDocumento"].Value = documento_result.DocumentoTipo.GetHashCode();
+								break;
+							case 5:
+								reporte_pdf = new HGInetFacturaEReports.Facturas.Formato5();
+								reporte_pdf.ReportParameters["TipoDocumento"].Value = documento_result.DocumentoTipo.GetHashCode();
+								break;
+
+							default:
+								switch (documento_result.DocumentoTipo)
+								{
+									case TipoDocumento.Factura:
+										reporte_pdf = new HGInetFacturaEReports.Facturas.Formato1();
+										break;
+									case TipoDocumento.NotaDebito:
+										reporte_pdf = new HGInetFacturaEReports.NotasDebito.Formato1();
+										break;
+									case TipoDocumento.NotaCredito:
+										reporte_pdf = new HGInetFacturaEReports.NotasCredito.Formato1();
+										break;
+								}
+								break;
+						}
+					}
+
+					if (reporte_archivado)
+					{
+						//Asigna los datos al reporte y genera el pdf
+						reporte_pdf.DataSource = documento_obj;
+						HGInetFacturaEReports.Reporte x = new HGInetFacturaEReports.Reporte(documento_result.NombreXml, documento_result.RutaArchivosEnvio);
+						x.GenerarPdf(reporte_pdf);
+
 					}
 
 					respuesta.UrlPdf = string.Format(@"{0}/{1}/{2}.pdf", url_ppal_pdf, LibreriaGlobalHGInet.Properties.RecursoDms.CarpetaFacturaEDian, documento_result.NombreXml);
+					respuesta.IdEstado = CategoriaEstado.Recibido.GetHashCode();
+					respuesta.DescripcionEstado = Enumeracion.GetDescription(CategoriaEstado.Recibido);
 
+					//Actualiza el registro en la base de datos.
+					documentoBd.StrUrlArchivoPdf = respuesta.UrlPdf;
+					documentoBd.IdCategoriaEstado = CategoriaEstado.Recibido.GetHashCode();
 				}
 
-				respuesta.DescripcionProceso = Enumeracion.GetDescription(Enumeracion.GetEnumObjectByValue<ProcesoEstado>(ProcesoEstado.PDFAlmacenamiento.GetHashCode()));
-				respuesta.FechaUltimoProceso = Fecha.GetFecha();
-				respuesta.IdProceso = ProcesoEstado.PDFAlmacenamiento.GetHashCode();
-				respuesta.IdEstado = Ctl_Documento.ObtenerCategoria(respuesta.IdProceso);
 
-				//Actualiza el registro en la base de datos.
-				documentoBd.StrUrlArchivoPdf = respuesta.UrlPdf;
-				documentoBd.IdCategoriaEstado = CategoriaEstado.Recibido.GetHashCode();
 
 			}
 			catch (Exception excepcion)
