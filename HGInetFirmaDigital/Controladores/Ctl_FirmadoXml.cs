97d9f4135aa354ad271c5fe9ab8ebf6d23e14ed6
Mparamo <mparamo@noemail.com>
2019-06-07 22:53:08 +0000

    Se agrega el firmado del documento XML para la versión con Validación Previa
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@1538 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetFirmaDigital/Controladores/Ctl_FirmadoXml.cs
+++ HGInetFirmaDigital/Controladores/Ctl_FirmadoXml.cs
@@ -1,27 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
 using System.EnterpriseServices;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Windows.Forms;
 using System.Xml;
-
 using FirmaXadesNet;
-using FirmaXadesNet.Signature;
 using FirmaXadesNet.Signature.Parameters;
 using FirmaXadesNet.Crypto;
 using HGInetFirmaDigital.Properties;
 using LibreriaGlobalHGInet.General;
-using System.Security.Cryptography.Xml;
 using LibreriaGlobalHGInet.Funciones;
 using FirmaXadesNet.Utils;
 using LibreriaGlobalHGInet.Objetos;
@@ -43,7 +31,7 @@ namespace HGInetFirmaDigital
 		/// <param name="empresa_certificadora">empresa certificadora</param>
 		/// <param name="rutas">Archivos XML para firmar</param>
 		/// <returns>Archivos Xml procesados</returns>  
-		public static List<FacturaE_Documento> FirmarDocumentos(string certificado_ruta, string certificado_serial, string certificado_clave, EnumCertificadoras empresa_certificadora, List<FacturaE_Documento> datos)
+		public static FacturaE_Documento FirmarDocumentos(string certificado_ruta, string certificado_serial, string certificado_clave, EnumCertificadoras empresa_certificadora, FacturaE_Documento datos)
 		{
 			try
 			{
@@ -52,12 +40,24 @@ namespace HGInetFirmaDigital
 
 				Ctl_FirmadoXml Firmar = new Ctl_FirmadoXml();
 
-				// firmado de archivos XML desde el almacen de certificados
-				if (string.IsNullOrEmpty(certificado_ruta))
-					datos = Firmar.FirmarXAdesAlmacen(certificado_serial, certificado_clave, empresa_certificadora, datos);
-				// firmado de archivos XML desde un certificado físico
+				if (datos.VersionDian == 2)
+				{	
+					// firmado de archivos XML desde el almacen de certificados
+					if (string.IsNullOrEmpty(certificado_ruta))
+						datos = Firmar.FirmarXAdesAlmacen(certificado_serial, certificado_clave, empresa_certificadora, datos);
+					// firmado de archivos XML desde un certificado físico
+					else
+						datos = Firmar.FirmarXAdesFisico_v2(certificado_ruta, certificado_serial, certificado_clave, empresa_certificadora, datos);
+				}
 				else
-					datos = Firmar.FirmarXAdesFisico(certificado_ruta, certificado_serial, certificado_clave, empresa_certificadora, datos);
+				{
+					// firmado de archivos XML desde el almacen de certificados
+					if (string.IsNullOrEmpty(certificado_ruta))
+						datos = Firmar.FirmarXAdesAlmacen(certificado_serial, certificado_clave, empresa_certificadora, datos);
+					// firmado de archivos XML desde un certificado físico
+					else
+						datos = Firmar.FirmarXAdesFisico(certificado_ruta, certificado_serial, certificado_clave, empresa_certificadora, datos);
+				}
 
 				return datos;
 			}
@@ -78,7 +78,7 @@ namespace HGInetFirmaDigital
 		/// <param name="rutas_archivos">Archivos XML para firmar</param>
 		/// <returns>Archivos Xml procesados</returns>
 		[AutoComplete(true)]
-		protected List<FacturaE_Documento> FirmarXAdesFisico(string RutaCertificado, string SerieCertificado, string ClaveCertificado, EnumCertificadoras EmpresaCertificadora, List<FacturaE_Documento> datos)
+		protected FacturaE_Documento FirmarXAdesFisico(string RutaCertificado, string SerieCertificado, string ClaveCertificado, EnumCertificadoras EmpresaCertificadora, FacturaE_Documento datos)
 		{
 			X509Certificate2 MontCertificat = null;
 			SignatureParameters parametros = null;
@@ -88,7 +88,7 @@ namespace HGInetFirmaDigital
 				{
 					throw new ApplicationException(string.Format("No se encuentra el certificado en la ruta {0}", RutaCertificado));
 				}
-				
+
 				XadesService xadesService = new XadesService();
 				parametros = new SignatureParameters();
 
@@ -133,13 +133,13 @@ namespace HGInetFirmaDigital
 				// Política de firma 
 				parametros.SignaturePolicyInfo = new SignaturePolicyInfo();
 				//parametros.SignaturePolicyInfo.PolicyDigestAlgorithm = DigestMethod.SHA1;
-				
+
 				// Política DIAN v1
 				//parametros.SignaturePolicyInfo.PolicyIdentifier = "https://facturaelectronica.dian.gov.co/politicadefirma/v1/politicadefirmav1.pdf";
 				//parametros.SignaturePolicyInfo.PolicyHash = "61fInBICBQOCBwuTwlaOZSi9HKc=";
 
 				// Política DIAN v2
-				parametros.SignaturePolicyInfo.PolicyIdentifier = "https://facturaelectronica.dian.gov.co/politicadefirma/v2/politicadefirmav2.pdf"; 
+				parametros.SignaturePolicyInfo.PolicyIdentifier = "https://facturaelectronica.dian.gov.co/politicadefirma/v2/politicadefirmav2.pdf";
 				parametros.SignaturePolicyInfo.PolicyHash = "sbcECQ7v+y/m3OcBCJyvmkBhtFs=";
 
 				/* GENERACIÓN PolicyHash - El valor lo generó Carlos Aguilar en Visual FoxPro
@@ -204,7 +204,7 @@ namespace HGInetFirmaDigital
 		/// <param name="rutas_archivos">Archivos XML para firmar</param>
 		/// <returns>Archivos Xml procesados</returns>
 		[AutoComplete(true)]
-		protected List<FacturaE_Documento> FirmarXAdesAlmacen(string SerieCertificado, string ClaveCertificado, EnumCertificadoras EmpresaCertificadora, List<FacturaE_Documento> datos)
+		protected FacturaE_Documento FirmarXAdesAlmacen(string SerieCertificado, string ClaveCertificado, EnumCertificadoras EmpresaCertificadora, FacturaE_Documento datos)
 		{
 
 			try
@@ -304,12 +304,9 @@ namespace HGInetFirmaDigital
 				}
 				else
 				{
-					// recorre los archivos para firmar
-					foreach (FacturaE_Documento archivo in datos)
-					{
-						if (archivo.Excepcion == null)
-							archivo.Excepcion = new ApplicationException("No se encuentra el certificado");
-					}
+					if (datos.Excepcion == null)
+						datos.Excepcion = new ApplicationException("No se encuentra el certificado");
+
 				}
 
 				// Cerramos el almacén de certificados
@@ -326,71 +323,215 @@ namespace HGInetFirmaDigital
 		}
 
 		/// <summary>
+		/// Firmado de varios archivos XML a través del certificado físico
+		/// </summary>
+		/// <param name="RutaCertificado">Ruta física del archivo del Certificado "C:/Certificado.pfx";</param>
+		/// <param name="SerieCertificado">Número de Serie del Certificado "‎‎74 c7 43 04 a4 ac 86 b6"</param>
+		/// <param name="ClaveCertificado">Clave del Certificado</param>
+		/// <param name="EmpresaCertificadora">Empresa que generó el certificado</param>
+		/// <param name="rutas_archivos">Archivos XML para firmar</param>
+		/// <returns>Archivos Xml procesados</returns>
+		[AutoComplete(true)]
+		protected FacturaE_Documento FirmarXAdesFisico_v2(string RutaCertificado, string SerieCertificado, string ClaveCertificado, EnumCertificadoras EmpresaCertificadora, FacturaE_Documento datos)
+		{
+			X509Certificate2 MontCertificat = null;
+			SignatureParameters parametros = null;
+			try
+			{
+				if (!Archivo.ValidarExistencia(RutaCertificado))
+				{
+					throw new ApplicationException(string.Format("No se encuentra el certificado en la ruta {0}", RutaCertificado));
+				}
+
+				XadesService xadesService = new XadesService();
+				parametros = new SignatureParameters();
+
+				parametros.SignatureMethod = SignatureMethod.RSAwithSHA256;
+				parametros.DigestMethod = DigestMethod.SHA256;
+
+				parametros.SignerRole = new SignerRole();
+				parametros.SignerRole.ClaimedRoles.Add("third party");
+
+				parametros.DatoIssuername = "";
+				parametros.DatoIssuername1 = "";
+				parametros.DatoIssuername0 = "";
+
+				// parámetros propios de la empresa que provee el certificado digital
+				if (EmpresaCertificadora == EnumCertificadoras.Gse)
+				{
+					parametros.DatoIssuername0 = CertificadorasDatos.GSE_DatoIssuername0;
+					parametros.DatoIssuername1 = CertificadorasDatos.GSE_DatoIssuername1;
+					parametros.DatoIssuername = CertificadorasDatos.GSE_DatoIssuername;
+				}
+				else if (EmpresaCertificadora == EnumCertificadoras.Andes)
+				{
+					parametros.DatoIssuername0 = CertificadorasDatos.Andes_DatoIssuername0;
+					parametros.DatoIssuername1 = CertificadorasDatos.Andes_DatoIssuername1;
+					parametros.DatoIssuername = CertificadorasDatos.Andes_DatoIssuername;
+				}
+
+				//inserta en nodo
+				parametros.SignatureDestination = new SignatureXPathExpression();
+				parametros.SignatureDestination.Namespaces.Add("cac", "urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2");
+				parametros.SignatureDestination.Namespaces.Add("cbc", "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2");
+				parametros.SignatureDestination.Namespaces.Add("ext", "urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2");
+				parametros.SignatureDestination.Namespaces.Add("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+				parametros.SignatureDestination.Namespaces.Add("xsd", "http://www.w3.org/2001/XMLSchema");
+				parametros.SignatureDestination.Namespaces.Add("xades", "http://uri.etsi.org/01903/v1.3.2#");
+				parametros.SignatureDestination.Namespaces.Add("xades141", "http://uri.etsi.org/01903/v1.4.1#");
+				parametros.SignatureDestination.Namespaces.Add("sts", "dian:gov:co:facturaelectronica:Structures-2-1");
+				parametros.SignatureDestination.Namespaces.Add("schemaLocation", "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2 http://docs.oasis-open.org/ubl/os-UBL-2.1/xsd/maindoc/UBL-Invoice-2.1.xsd");
+
+				/*
+				parametros.SignatureDestination.Namespaces.Add("fe", "http://www.dian.gov.co/contratos/facturaelectronica/v1");
+				parametros.SignatureDestination.Namespaces.Add("cac", "urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2");
+				parametros.SignatureDestination.Namespaces.Add("cbc", "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2");
+				parametros.SignatureDestination.Namespaces.Add("clm54217", "urn:un:unece:uncefact:codelist:specification:54217:2001");
+				parametros.SignatureDestination.Namespaces.Add("clm66411", "urn:un:unece:uncefact:codelist:specification:66411:2001");
+				parametros.SignatureDestination.Namespaces.Add("clmIANAMIMEMediaType", "urn:un:unece:uncefact:codelist:specification:IANAMIMEMediaType:2003");
+				parametros.SignatureDestination.Namespaces.Add("ext", "urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2");
+				parametros.SignatureDestination.Namespaces.Add("qdt", "urn:oasis:names:specification:ubl:schema:xsd:QualifiedDatatypes-2");
+				parametros.SignatureDestination.Namespaces.Add("sts", "http://www.dian.gov.co/contratos/facturaelectronica/v1/Structures");
+				parametros.SignatureDestination.Namespaces.Add("udt", "urn:un:unece:uncefact:data:specification:UnqualifiedDataTypesSchemaModule:2");
+				parametros.SignatureDestination.Namespaces.Add("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+				*/
+
+
+				// Política de firma 
+				parametros.SignaturePolicyInfo = new SignaturePolicyInfo();
+				parametros.SignaturePolicyInfo.PolicyDigestAlgorithm = DigestMethod.SHA256;
+
+				// Política DIAN v1
+				//parametros.SignaturePolicyInfo.PolicyIdentifier = "https://facturaelectronica.dian.gov.co/politicadefirma/v1/politicadefirmav1.pdf";
+				//parametros.SignaturePolicyInfo.PolicyHash = "61fInBICBQOCBwuTwlaOZSi9HKc=";
+
+				// Política DIAN v2
+				parametros.SignaturePolicyInfo.PolicyIdentifier = "https://facturaelectronica.dian.gov.co/politicadefirma/v2/politicadefirmav2.pdf";
+
+				string pdf_b64_sha256 = "dMoMvtcG5aIzgYo0tIsSQeVJBDnUnfSOfBpxXrmor0Y="; // https://hash.online-convert.com/es/generador-sha256
+
+				string pdf_b64_sha512 = "Zcjw1Z9nGQn2j6NyGx8kAaLbOfJGd/fJxRTCeirlqAg7zRG27piJkJOpflGu7XACpMj9hC6dVMcCyzqHxxPZeQ=="; //https://hash.online-convert.com/es/generador-sha512
+
+				parametros.SignaturePolicyInfo.PolicyHash = pdf_b64_sha256;
+
+
+				/* GENERACIÓN PolicyHash - El valor lo generó Carlos Aguilar en Visual FoxPro
+					SET LIBRARY TO LOCFILE("vfpencryption.fll")
+					cufehex=STRCONV(Hash(FILETOSTR("E:\FECOLOMBIA\politicadefirmav1.pdf"), 1), 15) && Easy way to get the hexBinary equivalent
+					cufehex =Hash(FILETOSTR("E:\FECOLOMBIA\politicadefirmav1.pdf"), 1) && Easy way to get the hexBinary equivalent
+					cufehex1=Hash(FILETOSTR("E:\FECOLOMBIA\politicadefirmav2.pdf"), 1)
+
+					?STRCONV(cufehex,13)
+
+					?STRCONV(cufehex1,13)
+				 */
+
+				parametros.cDescripcionPolicy = "Política de firma para facturas electrónicas de la República de Colombia";
+				parametros.SignaturePackaging = SignaturePackaging.ENVELOPED;
+				parametros.InputMimeType = "text/xml";
+
+				//"E:/certificadrdb/Certificado_Sunat_PFX_REPDORABEATRIZ.pfx";
+				MontCertificat = new X509Certificate2(RutaCertificado, ClaveCertificado);
+
+				parametros.cNombreCertificado = RutaCertificado;
+				parametros.cClaveCertificado = ClaveCertificado;
+				parametros.cdesdealmacen = "NO";
+
+			}
+			catch (Exception excepcion)
+			{
+
+				string msg = string.Format("Error HGInet Firmado al leer el certificado {1} de {3} clave:{2} - Excepción: {0}", excepcion.Message, RutaCertificado, ClaveCertificado, Enumeracion.GetDescription(EmpresaCertificadora));
+
+				if (excepcion.InnerException != null)
+					msg += " Inner: " + excepcion.InnerException.Message;
+
+				throw new ApplicationException(msg, excepcion);
+			}
+
+
+			try
+			{
+
+				// generar firma en el StringBuilder
+				datos = Firmar(datos, parametros, MontCertificat);
+
+			}
+			catch (Exception excepcion)
+			{
+
+				string msg = string.Format("Error HGInet Firmado - Excepción: {0}", excepcion.Message);
+
+				throw new ApplicationException(msg, excepcion);
+			}
+
+			return datos;
+		}
+
+
+		/// <summary>
 		/// valida y firma de documentos XML 
 		/// </summary>
 		/// <param name="data"></param>
 		/// <param name="parametros"></param>
 		/// <param name="MontCertificat"></param>
 		/// <returns></returns>
-		protected List<FacturaE_Documento> Firmar(List<FacturaE_Documento> data, SignatureParameters parametros, X509Certificate2 MontCertificat)
+		protected FacturaE_Documento Firmar(FacturaE_Documento archivo, SignatureParameters parametros, X509Certificate2 MontCertificat)
 		{
 			XadesService xadesService = new XadesService();
 
 			string archivo_xml = string.Empty;
 
-			// recorre los archivos para firmar
-			foreach (FacturaE_Documento archivo in data)
+			// valida que el archivo no tenga errores para procesarlo
+			if (archivo.Excepcion == null)
 			{
-				// valida que el archivo no tenga errores para procesarlo
-				if (archivo.Excepcion == null)
+				try
 				{
-					try
+					// valida el directorio de los archivos xml firmados
+					archivo.RutaArchivosProceso = Directorio.CrearDirectorio(archivo.RutaArchivosProceso);
+
+					archivo_xml = string.Format(@"{0}{1}.xml", archivo.RutaArchivosProceso, archivo.NombreXml);
+
+					// lee el archivo xml para firmar
+					using (FileStream fs = new FileStream(archivo_xml, FileMode.Open))
 					{
-						// valida el directorio de los archivos xml firmados
-						archivo.RutaArchivosProceso = Directorio.CrearDirectorio(archivo.RutaArchivosProceso);
+						// elemento donde se encontrará la firma de acuerdo con la raíz del documento xml
+						XmlDocument documentoXml = XMLUtil.LoadDocument(fs);
+						parametros.SignatureDestination.XPathExpression = string.Format("/{0}/ext:UBLExtensions/ext:UBLExtension[2]/ext:ExtensionContent", documentoXml.DocumentElement.Name);
 
-						archivo_xml = string.Format(@"{0}{1}.xml", archivo.RutaArchivosProceso, archivo.NombreXml);
-
-						// lee el archivo xml para firmar
-						using (FileStream fs = new FileStream(archivo_xml, FileMode.Open))
+						// lee el certificado digital para firmar
+						using (parametros.Signer = new Signer(MontCertificat))
 						{
-							// elemento donde se encontrará la firma de acuerdo con la raíz del documento xml
-							XmlDocument documentoXml = XMLUtil.LoadDocument(fs);
-							parametros.SignatureDestination.XPathExpression = string.Format("/{0}/ext:UBLExtensions/ext:UBLExtension[2]/ext:ExtensionContent", documentoXml.DocumentElement.Name);
+							string fecha = Fecha.GetFecha().AddMinutes(-1).ToString("yyy-MM-dd'T'HH:mm:ss.fffK");
+							parametros.SigningDate = Convert.ToDateTime(fecha);
 
-							// lee el certificado digital para firmar
-							using (parametros.Signer = new Signer(MontCertificat))
-							{
-								string fecha = Fecha.GetFecha().AddMinutes(-1).ToString("yyy-MM-dd'T'HH:mm:ss.fffK");
-								parametros.SigningDate = Convert.ToDateTime(fecha);
+							// firma el documento xml
+							var docFirmado = xadesService.Sign(fs, parametros, documentoXml);
 
-								// firma el documento xml
-								var docFirmado = xadesService.Sign(fs, parametros, documentoXml);
+							archivo_xml = string.Format(@"{0}\{1}.xml", archivo.RutaArchivosEnvio, archivo.NombreXml);
 
-								archivo_xml = string.Format(@"{0}\{1}.xml", archivo.RutaArchivosEnvio, archivo.NombreXml);
+							// almacena el documento xml con firma
+							docFirmado.Save(archivo_xml);
 
-								// almacena el documento xml con firma
-								docFirmado.Save(archivo_xml);
-
-								archivo.Excepcion = null;
-							}
+							archivo.Excepcion = null;
 						}
 					}
-					catch (Exception excepcion)
-					{
-						string msg = string.Format("Firmando el documento {0} - Excepción: {1}", archivo_xml, excepcion.Message);
+				}
+				catch (Exception excepcion)
+				{
+					string msg = string.Format("Firmando el documento {0} - Excepción: {1}", archivo_xml, excepcion.Message);
 
-						if (excepcion.InnerException != null)
-							msg += "  - " + excepcion.InnerException.Message;
+					if (excepcion.InnerException != null)
+						msg += "  - " + excepcion.InnerException.Message;
 
-						msg += " - " + excepcion.StackTrace.ToString();
+					msg += " - " + excepcion.StackTrace.ToString();
 
-						archivo.Excepcion = new ApplicationException(msg, excepcion);
-					}
+					archivo.Excepcion = new ApplicationException(msg, excepcion);
 				}
 			}
 
-			return data;
+
+			return archivo;
 		}
 
 	}
