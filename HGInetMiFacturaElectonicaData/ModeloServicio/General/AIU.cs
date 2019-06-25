e8dd99854b36ba39f3a97ca9dc6bf9167d6a5547
Jzea <jzea.hgi@gmail.com>
2019-06-25 16:48:09 +0000

    [464898] Propiedades para la nueva Version
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@1570 30a10932-8c21-4e4d-906a-15261f94da71

---
+++ HGInetMiFacturaElectonicaData/ModeloServicio/General/AIU.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HGInetMiFacturaElectonicaData.ModeloServicio.General
+{
+	/// <summary>
+	/// Objeto para manejo de AIU
+	/// </summary>
+	public class AIU
+	{
+
+		/// <summary>
+		/// Valor de la Administracion
+		/// </summary>
+		public decimal ValorAdministracion { get; set; }
+
+		/// <summary>
+		/// Valor del Imprevisto
+		/// </summary>
+		public decimal ValorImprevisto { get; set; }
+
+		/// <summary>
+		/// Valor de la Utilidad
+		/// </summary>
+		public decimal ValorUtilidad { get; set; }
+
+		/// <summary>
+		/// Valor del porcentaje que se calculo el valor de la utilidad
+		/// </summary>
+		public decimal ProcentajeUtilidad { get; set; }
+
+	}
+}
