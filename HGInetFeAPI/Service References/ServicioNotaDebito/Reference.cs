b4a30becb29b0e8b12c477f98f960fe07d7d44e3
Jzea <jzea.hgi@gmail.com>
2020-01-09 14:59:27 +0000

    -Actualizacion de Libreria
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@1922 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetFeAPI/Service References/ServicioNotaDebito/Reference.cs
+++ HGInetFeAPI/Service References/ServicioNotaDebito/Reference.cs
@@ -1352,7 +1352,7 @@ namespace HGInetFeAPI.ServicioNotaDebito {
     [System.Diagnostics.DebuggerStepThroughAttribute()]
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Runtime.Serialization", "4.0.0.0")]
     [System.Runtime.Serialization.DataContractAttribute(Name="ReferenciaAdicional", Namespace="http://schemas.datacontract.org/2004/07/HGInetMiFacturaElectonicaData.ModeloServi" +
-        "cio.Documentos")]
+        "cio")]
     [System.SerializableAttribute()]
     public partial class ReferenciaAdicional : object, System.Runtime.Serialization.IExtensibleDataObject, System.ComponentModel.INotifyPropertyChanged {
         
