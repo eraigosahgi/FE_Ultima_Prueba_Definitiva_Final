f0c9a0933c45463bf6b15c760163d2a583225b59
Jzea <jzea.hgi@gmail.com>
2020-01-14 22:28:16 +0000

    -Se agrega servicio de Listas de FE
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@1928 30a10932-8c21-4e4d-906a-15261f94da71

---
+++ HGInetMiFacturaElectronicaWeb/wcf/listaFe.svc.cs
@@ -0,0 +1,61 @@
+﻿using HGInetMiFacturaElectonicaData.ModeloServicio;
+using HGInetMiFacturaElectronicaWeb.Controllers.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Text;
+using HGInetMiFacturaElectonicaController.Configuracion;
+using LibreriaGlobalHGInet.Error;
+
+namespace HGInetMiFacturaElectronicaWeb.wcf
+{
+	[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+	[ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+	public class listaFe : IlistaFe
+	{
+		public string DoWork()
+		{
+			return "¡Prueba correcta!";
+		}
+
+		public List<ListaFE> Obtener(string DataKey, string Identificacion, string CodigoLista)
+		{
+			try
+			{
+
+				if (string.IsNullOrEmpty(DataKey))
+					throw new ApplicationException("Parámetro DataKey de tipo string inválido.");
+
+				if (string.IsNullOrEmpty(Identificacion))
+					throw new ApplicationException("Parámetro Identificacion de tipo string inválido.");
+
+				bool todas = false;
+
+				if (string.IsNullOrEmpty(CodigoLista) || CodigoLista.Equals("*"))
+					todas = true;
+
+
+				List<ListaFE> respuesta = new List<ListaFE>();
+
+				//Válida que la key sea correcta.
+				Peticion.Validar(DataKey, Identificacion);
+
+				Ctl_ListaFe ctl_lista = new Ctl_ListaFe();
+
+				// obtiene los datos
+				respuesta = ctl_lista.Obtener(CodigoLista, todas);
+
+				return respuesta;
+
+			}
+			catch (Exception exec)
+			{
+				Error error = new Error(CodigoError.VALIDACION, exec);
+				throw new FaultException<Error>(error, new FaultReason(string.Format("{0}", error.Mensaje)));
+			}
+		}
+	}
+}
--- HGInetUBLv2_1/Dian/DianListas/ListaItem.cs
+++ HGInetUBLv2_1/Dian/DianListas/ListaItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,33 @@ namespace HGInetUBLv2_1.DianListas
 			this.Codigo = codigo;
 			this.Nombre = nombre;
 			this.Descripcion = descripcion;
+		}
+
+		public enum ListasFE
+		{
+			[Description("Codigo Postal")]
+			CodPostal = 1,
+
+			[Description("Medios de Pago")]
+			MedioPago = 2,
+
+			[Description("Paises")]
+			Paises = 3,
+
+			[Description("Productos")]
+			Productos = 4,
+
+			[Description("Tipo Codigo Productos")]
+			TipoCodProductos = 5,
+
+			[Description("Tipo Entrega")]
+			TipoEntrega = 6,
+
+			[Description("Tipo Moneda")]
+			TipoMoneda = 7,
+
+			[Description("Unidades de Medida")]
+			UnidadMedida = 8,
 		}
 
 	}
