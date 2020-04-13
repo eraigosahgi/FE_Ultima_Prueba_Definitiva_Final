5d85b9535a215084c4b6339a8c10fd54fa2a3640
Jzea <jzea.hgi@gmail.com>
2020-04-13 16:14:20 +0000

    Se adecua que el ajuste se haga a versiones de erp menores a 2020.4
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@1980 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetUBLv2_1/XML/ImpuestosXML.cs
+++ HGInetUBLv2_1/XML/ImpuestosXML.cs
@@ -81,7 +81,8 @@ namespace HGInetUBLv2_1
 						imp_doc.BaseImponible += BaseimponibleMuestra;
 					}
 
-					if ((!version_erp.Equals(version_validar) && !version_erp.Contains(version_validar_Rev)) && hgi == -1)
+					//Ajuste del impuesto para Versiones de ERP menores a 2020.4
+					if ((!version_erp.Equals(version_validar) && !version_erp.Contains(version_validar)) && hgi == -1)
 					{
 						decimal imp_cal = decimal.Round(BaseImponibleImpuesto * (imp_doc.Porcentaje / 100), 2, MidpointRounding.AwayFromZero);
 
