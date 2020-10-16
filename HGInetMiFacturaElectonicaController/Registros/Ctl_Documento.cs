28983ec3de13c9fc5ce7d1708f9c8c5cd67f979c
Mparamo <mparamo@noemail.com>
2020-10-16 16:29:00 +0000

    Se restringe la consulta de documentos por 100 registros separados por comas de acuerdo con la estabilidad del servidor
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@2096 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectonicaController/Registros/Ctl_Documento.cs
+++ HGInetMiFacturaElectonicaController/Registros/Ctl_Documento.cs
@@ -261,6 +261,10 @@ namespace HGInetMiFacturaElectonicaController.Registros
 				//Convierte CodigoRegistros en una lista.
 				List<string> lista_documentos = Coleccion.ConvertirLista(CodigosRegistros);
 
+				//Se restringe la consulta a un número de documentos específicos
+				if(lista_documentos.Count > 100)
+					throw new ApplicationException("Supera el número máximo de 100 registros por consulta.");
+
 				context.Configuration.LazyLoadingEnabled = false;
 
 				var respuesta = from documento in context.TblDocumentos
