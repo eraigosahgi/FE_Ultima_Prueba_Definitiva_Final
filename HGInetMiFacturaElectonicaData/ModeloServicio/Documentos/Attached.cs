b21a6d1d4e2189d852dea272fe19c355e5523112
Jzea <jzea.hgi@gmail.com>
2020-12-07 15:21:11 +0000

    -Se agrega objeto para el attach y la clase para convertir el xml a objeto
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@2163 30a10932-8c21-4e4d-906a-15261f94da71

---
+++ HGInetMiFacturaElectonicaData/ModeloServicio/Documentos/Attached.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HGInetMiFacturaElectonicaData.ModeloServicio
+{
+	/// <summary>
+	/// Documento Tipo Attached(Contenedor)
+	/// </summary>
+	public class Attached
+	{
+		/// <summary>
+		///Consecutivo del attached. 
+		/// </summary>
+		public string IdAttached { get; set; }
+
+		/// <summary>
+		/// Fecha completa del Attached
+		/// </summary>
+		public DateTime Fecha { get; set; }
+
+		/// <summary>
+		/// Identificacion del facturador del documento
+		/// </summary>
+		public string IdentificacionFacturador { get; set; }
+
+		/// <summary>
+		/// Identificacion del facturador del documento
+		/// </summary>
+		public string Identificacionadquiriente { get; set; }
+
+		/// <summary>
+		/// Número del Documento electronico que se incluye en Attached
+		/// </summary>
+		public long Documento { get; set; }
+
+		/// <summary>
+		/// Prefijo del Documento
+		/// </summary>
+		public string Prefijo { get; set; }
+
+		/// <summary>
+		/// Documento Electronico en XML sin la Expresion CDATA
+		/// </summary>
+		public string DocumentoElectronico { get; set; }
+
+		/// <summary>
+		/// ApplicationResponse de la DIAN en XML sin la Expresion CDATA
+		/// </summary>
+		public string RespuestaDianXml { get; set; }
+
+		///// <summary>
+		///// Datos del Obligado a facturar
+		///// </summary>
+		//public Tercero DatosObligado { get; set; }
+
+		///// <summary>
+		///// Datos del Adquiriente de la Factura
+		///// </summary>
+		//public Tercero DatosAdquiriente { get; set; }
+
+		/// <summary>
+		/// Código de respuesta de acuerdo a los valores definidos en Guia de Interoperabilidad en la ​Tabla No. 5.2.1: Códigos de respuesta - Capítulo No. 5.2. 
+		/// </summary>
+		public string CufeDocumentoElectronico { get; set; }
+
+		/// <summary>
+		/// Descripción que complementa o justifica el código de respuesta reportado
+		/// </summary>
+		public string MvoRespuesta { get; set; }
+	}
+}
---
+++ HGInetUBLv2_1/XML/AttachedDocument.Convertir.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HGInetMiFacturaElectonicaData.ModeloServicio;
+
+namespace HGInetUBLv2_1
+{
+	public partial class AttachedDocument
+	{
+		public static Attached Convertir(AttachedDocumentType attach)
+		{
+			Attached attah_document = new Attached();
+
+
+
+
+
+
+			return attah_document;
+		}
+	}
+}
