9b6af1bdaa2280bf5adeef67b1b35a9ceec5e79f
Mparamo <mparamo@noemail.com>
2020-12-07 18:31:47 +0000

    - Se actualiza la versión de NewtonSoft - Se crea página web para pruebas de Interoperabilidad
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@2169 30a10932-8c21-4e4d-906a-15261f94da71

---
+++ HGInetMiFacturaElectronicaWeb/Tests/TestInteroperabilidad.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TestInteroperabilidad.aspx.cs" Inherits="HGInetMiFacturaElectronicaWeb.Tests.TestInteroperabilidad" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+		<asp:textbox runat="server" ID="txt_resultado" Height="500px" TextMode="MultiLine" Width="90%"></asp:textbox>
+    </div>
+    </form>
+</body>
+</html>
---
+++ HGInetMiFacturaElectronicaWeb/Tests/TestInteroperabilidad.aspx.cs
@@ -0,0 +1,26 @@
+﻿using HGInetInteroperabilidad.Procesos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HGInetMiFacturaElectronicaWeb.Tests
+{
+	public partial class TestInteroperabilidad : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			try
+			{
+				Ctl_MailRecepcion.Procesar();
+				txt_resultado.Text = "";
+			}
+			catch (Exception excepcion)
+			{
+				
+			}
+		}
+	}
+}
