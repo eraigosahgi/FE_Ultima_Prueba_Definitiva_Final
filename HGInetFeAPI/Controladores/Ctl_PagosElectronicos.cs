cc5bbf0e9aa7dbdd0652acdd70347174fe70e694
Jflores <jflores.hgi@gmail.com>
2022-02-07 22:12:28 +0000

    -Se agrega metodo de consulta de los pagos agrupados, en la libreria de integración.
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@2495 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetFeAPI/Controladores/Ctl_PagosElectronicos.cs
+++ HGInetFeAPI/Controladores/Ctl_PagosElectronicos.cs
@@ -181,6 +181,91 @@ namespace HGInetFeAPI
 			}
 		}
 
+		/// <summary>
+		/// Permite consultar el resultado de los pagos en la plataforma por el Facturador Electrónico agrupados por Ciclo PSE e individuales cuando es Tarjeta de Credito		
+		/// </summary>
+		/// <param name="UrlWs">ruta principal de ejecución del servicio web HGI Facturación Electrónica (http)</param>
+		/// <param name="Serial">serial de licenciamiento para HGI Facturación Electrónica</param>
+		/// <param name="Identificacion">número de identificación del Facturador Electrónico</param>
+		/// <param name="FechaInicial">Fecha Inicial</param>
+		/// <param name="FechaFinal">Fecha Final</param>
+		/// <param name="Procesados"></param>
+		/// <returns>indica el Pago fue procesado o no: 0 Todos Los Pagos, 1 Pagos Procesados, 2 Pagos que aun no se han Procesado</returns>
+		public static List<ServicioPagosElectronicos.PagoElectronicoRespuestaAgrupadoPorFecha> ConsultaAgrupadosPorFechaElaboracion(string UrlWs, string Serial, string Identificacion, DateTime FechaInicial, DateTime FechaFinal, int Procesados = 0)
+		{
+
+			// valida si es un integrador o son pruebas para que obtenga la ruta que le corresponda
+			if (!UrlWs.Contains("hgi"))
+			{
+				UrlWs = Ctl_Utilidades.ObtenerUrl(UrlWs, Identificacion);
+			}
+
+			// valida la URL del servicio web
+			UrlWs = string.Format("{0}{1}", Ctl_Utilidades.ValidarUrl(UrlWs), UrlWcf);
+
+			// valida el parámetro Serial
+			if (string.IsNullOrEmpty(Serial))
+				throw new ApplicationException("Parámetro Serial de tipo string inválido.");
+
+			// valida el parámetro Identificacion
+			if (string.IsNullOrEmpty(Identificacion))
+				throw new ApplicationException("Parámetro Identificacion de tipo string inválido.");
+
+			List<ServicioPagosElectronicos.PagoElectronicoRespuestaAgrupadoPorFecha> datos = new List<ServicioPagosElectronicos.PagoElectronicoRespuestaAgrupadoPorFecha>();
+
+			ServicioPagosElectronicos.ServicioPagosElectronicosClient cliente_ws = null;
+
+			try
+			{
+				// conexión cliente para el servicio web
+				EndpointAddress endpoint_address = new System.ServiceModel.EndpointAddress(UrlWs);
+				cliente_ws = new ServicioPagosElectronicos.ServicioPagosElectronicosClient(Ctl_Utilidades.ObtenerBinding(UrlWs), endpoint_address);
+				cliente_ws.Endpoint.Address = new System.ServiceModel.EndpointAddress(UrlWs);
+
+				// configura la cadena de autenticación para la ejecución del servicio web en SHA1
+				string dataKey = Ctl_Utilidades.Encriptar_SHA512(string.Format("{0}{1}", Serial, Identificacion));
+
+				// datos para la petición
+				ServicioPagosElectronicos.ConsultaAgrupadosPorFechaElaboracionRequest peticion = new ServicioPagosElectronicos.ConsultaAgrupadosPorFechaElaboracionRequest()
+				{
+					DataKey = dataKey,
+					Identificacion = Identificacion,
+					FechaInicial = FechaInicial,
+					FechaFinal = FechaFinal,
+					Procesados = Procesados
+				};
+
+				// ejecución del servicio web
+				ServicioPagosElectronicos.ConsultaAgrupadosPorFechaElaboracionResponse respuesta = cliente_ws.ConsultaAgrupadosPorFechaElaboracion(peticion);
+
+				// resultado del servicio web
+				List<ServicioPagosElectronicos.PagoElectronicoRespuestaAgrupadoPorFecha> result = respuesta.ConsultaAgrupadosPorFechaElaboracionResult;
+
+				if (respuesta != null)
+					return result;
+				else
+					throw new Exception("Error al obtener los datos con los parámetros indicados.");
+
+			}
+			catch (FaultException excepcion)
+			{
+				throw new ApplicationException(excepcion.Message, excepcion);
+			}
+			catch (CommunicationException excepcion)
+			{
+				throw new Exception(string.Format("Error de comunicación: {0}", excepcion.Message), excepcion);
+			}
+			catch (Exception excepcion)
+			{
+				throw excepcion;
+			}
+			finally
+			{
+				if (cliente_ws != null)
+					cliente_ws.Abort();
+			}
+		}
+
 
 
 		/// <summary>
