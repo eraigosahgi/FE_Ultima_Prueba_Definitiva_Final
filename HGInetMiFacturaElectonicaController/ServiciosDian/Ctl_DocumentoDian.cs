3fcd7d6cd86809a23fb29cea0b5b67d972fa37ce
Jzea <jzea.hgi@gmail.com>
2022-08-16 17:22:32 +0000

    Ajuste en excepcion de envio de eventos a la DIAN
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@2736 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectonicaController/ServiciosDian/Ctl_DocumentoDian.cs
+++ HGInetMiFacturaElectonicaController/ServiciosDian/Ctl_DocumentoDian.cs
@@ -246,6 +246,7 @@ namespace HGInetMiFacturaElectonicaController.ServiciosDian
 													respuesta_dian.FirstOrDefault().ErrorMessage = LibreriaGlobalHGInet.Formato.Coleccion.ConvertirLista(respuesta_dian.FirstOrDefault().StatusDescription).ToArray();
 													respuesta_dian.FirstOrDefault().StatusCode = "99";
 													acuse.MessagesFieldV2 = new HGInetDIANServicios.DianWSValidacionPrevia.XmlParamsResponseTrackId[1];
+													acuse.MessagesFieldV2[0] = new HGInetDIANServicios.DianWSValidacionPrevia.XmlParamsResponseTrackId();
 													acuse.MessagesFieldV2[0].ProcessedMessage = respuesta_dian.FirstOrDefault().StatusDescription;
 												}
 											}
