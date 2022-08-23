5437c7cbd1a33abcab3ca177b5f49a42be5b4521
Jzea <jzea.hgi@gmail.com>
2022-08-23 18:32:04 +0000

    -Ajuste en proceso de generacion de evento en el XML
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@2746 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetUBLv2_1/XML/AcuseReciboXMLv2_1.cs
+++ HGInetUBLv2_1/XML/AcuseReciboXMLv2_1.cs
@@ -685,7 +685,7 @@ namespace HGInetUBLv2_1
 				{
 					//-----Se debe agregar validacion cuando es endoso la propiedad response.ResponseCode.listID si es 2 el calculo tiene un cambio
 					string code_response = string.Empty;
-					if (acuse.DocumentResponse != null && acuse.DocumentResponse[0].Response != null && acuse.DocumentResponse[0].Response.ResponseCode != null && (acuse.DocumentResponse[0].Response.ResponseCode.listID.Equals("2")) && !tipo_acuse.Equals(CodigoResponseV2.PagoFvTV) && !tipo_acuse.Equals(CodigoResponseV2.MandatoG))
+					if (acuse.DocumentResponse != null && acuse.DocumentResponse[0].Response != null && acuse.DocumentResponse[0].Response.ResponseCode != null && acuse.DocumentResponse[0].Response.ResponseCode.listID != null && (acuse.DocumentResponse[0].Response.ResponseCode.listID.Equals("2")) && !tipo_acuse.Equals(CodigoResponseV2.PagoFvTV) && !tipo_acuse.Equals(CodigoResponseV2.MandatoG))
 					{
 						code_response = acuse.DocumentResponse[0].Response.ResponseCode.listID;
 					}
