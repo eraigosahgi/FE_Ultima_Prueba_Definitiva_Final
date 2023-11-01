87febdc3c6364221d529046fb44cf8de5e7bc910
Jzea <jzea.hgi@gmail.com>
2023-11-01 00:31:17 +0000

    [727405] -Ajuste con librerias de zip
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@3004 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectronicaWeb/Controllers/Services/DocumentosController.cs
+++ HGInetMiFacturaElectronicaWeb/Controllers/Services/DocumentosController.cs
@@ -26,7 +26,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
-//using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Net;
