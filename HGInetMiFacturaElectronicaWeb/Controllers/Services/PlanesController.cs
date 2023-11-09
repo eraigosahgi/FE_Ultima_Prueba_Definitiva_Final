d8968bdc6bcc8068c3d01077caa9a3da4f45f8b0
Jflores <jflores.hgi@gmail.com>
2023-11-09 14:44:05 +0000

    -Se quita la validacion de 500 documentos maximos a recargar ya que el plan de happgi plus tiene 1.200 documentos y al comprar o renovar, este crea una recarga con este monto.
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@3011 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectronicaWeb/Controllers/Services/PlanesController.cs
+++ HGInetMiFacturaElectronicaWeb/Controllers/Services/PlanesController.cs
@@ -99,7 +99,7 @@ namespace HGInetMiFacturaElectronicaWeb.Controllers.Services
 
 				Ctl_PlanesTransacciones clase_planes = new Ctl_PlanesTransacciones();
 				
-				TblPlanesTransacciones postpago = clase_planes.Obtener(facturador).FirstOrDefault(x => x.IntEstado == 0 && (x.IntTipoProceso == 3 ));				
+				TblPlanesTransacciones postpago = clase_planes.Obtener(facturador).FirstOrDefault(x => x.IntEstado == 0 && (x.IntTipoProceso == 2 ));				
 
 				if (postpago == null)
 				{
@@ -120,7 +120,7 @@ namespace HGInetMiFacturaElectronicaWeb.Controllers.Services
 
 					//Se valida si el numero de documentos comprados es mayor a 500 
 					// entonces lo estan haciendo fuera del proceso de happgi ya que este permite solo los planes de la pagina
-					if (ObjPlanTransacciones.IntNumTransaccCompra >= 500)
+					if (ObjPlanTransacciones.IntNumTransaccCompra > 1200)
 					{
 						return Ok("Compra de documentos supera la cantidad de documentos permitidos <br /><br /> Cualquier inquietud comunicarse con nuestra area de Soporte.");
 					}
