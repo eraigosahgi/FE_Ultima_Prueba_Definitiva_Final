9ec912065084d248a1630405d54fc1a4903627db
Jzea <jzea.hgi@gmail.com>
2024-04-17 17:44:18 +0000

    [760058] -Se ajusta para que el documento referencia no lleve guiones y tampoco espacios, se deja prefijo+numero para que lo permita subir en plataforma de Transporte RNDC
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@3096 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectonicaController/Procesos/Ctl_Documentos.NotaCredito.cs
+++ HGInetMiFacturaElectonicaController/Procesos/Ctl_Documentos.NotaCredito.cs
@@ -512,7 +512,7 @@ namespace HGInetMiFacturaElectonicaController.Procesos
 									if (doc_resp.IdVersionDian == 1)
 										item.TipoOperacion = 22;
 									//throw new ApplicationException(string.Format("El número de Factura afectada {0} no es válida para la Versión que se esta enviando", item.DocumentoRef));
-									item.DocumentoRef = string.IsNullOrWhiteSpace(doc_resp.Prefijo) ? doc_resp.Documento.ToString()  : string.Format("{0} - {1}", doc_resp.Prefijo, doc_resp.Documento);
+									item.DocumentoRef = string.IsNullOrWhiteSpace(doc_resp.Prefijo) ? doc_resp.Documento.ToString()  : string.Format("{0}{1}", doc_resp.Prefijo, doc_resp.Documento);
 								}
 								else
 								{
