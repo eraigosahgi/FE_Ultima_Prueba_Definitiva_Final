8159f26a8e8d1521ce34c76f74003f9b76a4c418
Jzea <jzea.hgi@gmail.com>
2024-05-03 19:31:48 +0000

    -Se agrega Validacion para que no permita recibir documentos que no tengan la Fecha Actual Anexo Tecnico V1.9
    git-svn-id: http://192.168.0.251:18080/svn/HGInetMiFacturaElectronica/Tronco@3115 30a10932-8c21-4e4d-906a-15261f94da71

--- HGInetMiFacturaElectonicaController/Procesos/Ctl_Documentos.Factura.cs
+++ HGInetMiFacturaElectonicaController/Procesos/Ctl_Documentos.Factura.cs
@@ -905,8 +905,8 @@ namespace HGInetMiFacturaElectonicaController.Procesos
 			}
 			else
 			{
-				if (documento.Fecha.Date < Fecha.GetFecha().AddDays(-10).Date || documento.Fecha.Date > Fecha.GetFecha().Date.AddDays(10))
-					throw new ApplicationException(string.Format("La fecha de elaboración {0} no puede ser 10 dias superior o inferior a la fecha actual.", documento.Fecha));
+				if (documento.Fecha.Date != Fecha.GetFecha().Date)
+					throw new ApplicationException(string.Format("La fecha de elaboración {0} no puede ser diferente a la fecha actual.", documento.Fecha));
 
 				if (documento.FormaPago != 0)
 				{
--- HGInetMiFacturaElectonicaController/Procesos/Ctl_Documentos.NotaCredito.cs
+++ HGInetMiFacturaElectonicaController/Procesos/Ctl_Documentos.NotaCredito.cs
@@ -986,8 +986,8 @@ namespace HGInetMiFacturaElectonicaController.Procesos
 			}
 			else
 			{
-				if (documento.Fecha.Date < Fecha.GetFecha().AddDays(-10).Date || documento.Fecha.Date > Fecha.GetFecha().Date.AddDays(10))
-					throw new ApplicationException(string.Format("La fecha de elaboración {0} no puede ser 10 dias superior o inferior a la fecha actual.", documento.Fecha));
+				if (documento.Fecha.Date != Fecha.GetFecha().Date)
+					throw new ApplicationException(string.Format("La fecha de elaboración {0} no puede ser diferente a la fecha actual.", documento.Fecha));
 
 				ListaConceptoNotaCredito list_concepto = new ListaConceptoNotaCredito();
 				ListaItem concepto = list_concepto.Items.Where(d => d.Codigo.Equals(documento.Concepto)).FirstOrDefault();
